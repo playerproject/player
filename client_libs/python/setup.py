64c695f2cf45fb472851208df565f82ac123d006
Boyoon Jung <boyoonj@gmail.com>
2001-04-05 04:24:14 +0000

    Add a python module for Player.

---
+++ client_libs/python/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup (name = "PlayerClient",
+       version = "0.1",
+       description = "Player Client",
+       author = "Boyoon Jung",
+       author_email = "boyoon@robotics.usc.edu",
+       py_modules = ["PlayerClient"])
