b5b76a2754db0320b359be74b2cccb9bcd5719b4
Andrew Howard <ahoward@usc.edu>
2001-05-11 02:38:54 +0000

    Complete re-write of laser beacon detector.  Now works more reliably, and at longer range.

--- src/laserbeacondevice.cc
+++ src/laserbeacondevice.cc
@@ -37,9 +37,8 @@
 // Theory of operation:
 //  Will detect binary coded beacons (i.e. bar-codes) in laser data.
 //  Reflectors represent '1' bits, non-reflectors represent '0' bits.
-//  The first and last bits of the beacon must be '1'; these bits
-//  are used to locate candidate beacons, determine their size and orientation.
-//  Intermediate bits can be used to encode a unique id.
+//  The first and second bits of the beacon must be '1' and '0' respectively.
+//  More significant bits can be used to encode a unique id.
 //
 //  Will return an id of zero if it can see a beacon, but cannot identify it.
 //
@@ -227,76 +226,62 @@ int CLaserBeaconDevice::Shutdown()
 void CLaserBeaconDevice::FindBeacons(const player_laser_data_t *laser_data,
                                      player_laserbeacon_data_t *beacon_data)
 {
-    //TRACE0("searching");
-    
     beacon_data->count = 0;
 
-    int marker_count = 0;
-    double marker[PLAYER_NUM_LASER_SAMPLES][3];
-
-    // Extract all the markers (reflectors)
-    //
+    double max_width = m_max_bits * m_bit_width;
+    
+    int ai = -1;
+    int bi = -1;
+    double ax, ay;
+    double bx, by;
+    
     for (int i = 0; i < laser_data->range_count; i++)
     {
         int intensity = (int) (laser_data->ranges[i] >> 13);
         double range = (double) (laser_data->ranges[i] & 0x1FFF) / 1000;
         double bearing = (double) (laser_data->min_angle + i * laser_data->resolution)
             / 100.0 * M_PI / 180;
-         
+
+        double px = range * cos(bearing);
+        double py = range * sin(bearing);
+        
         if (intensity > 0)
         {
-            marker[marker_count][0] = i;
-            marker[marker_count][1] = range;
-            marker[marker_count][2] = bearing;
-            marker_count++;
+            if (ai < 0)
+            {
+                ai = i;
+                ax = px;
+                ay = py;
+            }
         }
-    }
+        if (ai < 0)
+            continue;
+        
+        double width = sqrt((px - ax) * (px - ax) + (py - ay) * (py - ay));
+        if (width < max_width)
+            continue;
 
-    // Now look for pairs of markers that are the correct
-    // distance apart
-    //
-    for (int i = 0; i < marker_count; i++)
-    {
-        int ai = (int) marker[i][0];
-        double arange = marker[i][1];
-        double abearing = marker[i][2];
-        double ax = marker[i][1] * cos(marker[i][2]);
-        double ay = marker[i][1] * sin(marker[i][2]);
-            
-        for (int j = i + 1; j < marker_count; j++)
+        bi = i;
+        bx = px;
+        by = py;
+        
+        printf("%d %d\n", ai, bi);
+        double orient = atan2(by - ay, bx - ax);
+        int id = IdentBeacon(ai, bi, ax, ay, orient, laser_data);
+        ai = -1;
+        bi = -1;
+
+        if (id < 0)
+            continue;
+        
+        // Check for array overflow
+        //
+        if (beacon_data->count < ARRAYSIZE(beacon_data->beacon))
         {
-            int bi = (int) marker[j][0];
-            double brange = marker[j][1];
-            double bbearing = marker[j][2];
-            double bx = marker[j][1] * cos(marker[j][2]);
-            double by = marker[j][1] * sin(marker[j][2]);
-
-            double dx = bx - ax;
-            double dy = by - ay;
-            double width = sqrt(dx * dx + dy * dy);
-            double orient = atan2(dy, dx);
-
-            // Check the width
-            //
-            if (width < m_min_width || width > m_max_width)
-                continue;
-
-            // Assign an id to this candidate beacon
-            // Will return -1 if this is not a beacon.
-            // Will return  0 if this is a beacon, but it cannot be identified.
-            // Will return beacon id otherwise
-            //
-            int id = IdentBeacon(ai, bi, ax, ay, orient, laser_data);
-            if (id < 0)
-                continue;
-
-            // Check for array overflow
-            //
-            if (beacon_data->count >= ARRAYSIZE(beacon_data->beacon))
-                continue;
-            
-            double range = (arange + brange) / 2;
-            double bearing = (abearing + bbearing) / 2;
+            double ox = (bx + ax) / 2;
+            double oy = (by + ay) / 2;
+            double range = sqrt(ox * ox + oy * oy);
+            double bearing = atan2(oy, ox);
 
             // Create an entry for this beacon
             // Note that we return the surface normal for the beacon orientation.
@@ -320,11 +305,9 @@ void CLaserBeaconDevice::FindBeacons(const player_laser_data_t *laser_data,
 int CLaserBeaconDevice::IdentBeacon(int a, int b, double ox, double oy, double oth,
                                     const player_laser_data_t *laser_data)
 {
-    int hist[PLAYER_NUM_LASER_SAMPLES];
-    memset(hist, 0, sizeof(hist));
-    
-    //printf("candidate %d:%d  ", (int) a, (int) b);
-    
+    int term_count = 0;
+    double term[PLAYER_NUM_LASER_SAMPLES][2];
+
     // Scan through the readings that make up the candidate
     //
     for (int i = a; i <= b; i++)
@@ -333,80 +316,88 @@ int CLaserBeaconDevice::IdentBeacon(int a, int b, double ox, double oy, double o
         double range = (double) (laser_data->ranges[i] & 0x1FFF) / 1000;
         double bearing = (double) (laser_data->min_angle + i * laser_data->resolution)
             / 100.0 * M_PI / 180;
+        double res = (double) laser_data->resolution / 100.0 * M_PI / 180;
         
-        double tx = range * cos(bearing) - ox;
-        double ty = range * sin(bearing) - oy;
+        double cx = range * cos(bearing) - ox;
+        double cy = range * sin(bearing) - oy;
+
+        double ax = range * cos(bearing - res) - ox;
+        double ay = range * sin(bearing - res) - oy;
+
+        double bx = range * cos(bearing + res) - ox;
+        double by = range * sin(bearing + res) - oy;
 
         // Rotate so that points all lie along x-axis
         // with the origin at the first point
         //
-        double px = tx * cos(-oth) - ty * sin(-oth);
-        double py = tx * sin(-oth) + ty * cos(-oth);
+        double tcx = cx * cos(-oth) - cy * sin(-oth);
+        double tcy = cx * sin(-oth) + cy * cos(-oth);
+        double tax = ax * cos(-oth) - ay * sin(-oth);
+        double tbx = bx * cos(-oth) - by * sin(-oth);
 
         // Discard candidate points are not close to x-axis
         // (ie candidate is not flat).
         //
-        if (fabs(py) > m_max_depth)
+        if (fabs(tcy) > m_max_depth)
             return -1;
 
-        // Work out which bit this point corresponds to
-        //
-        int bin = (int) (px / m_bit_width);
-        if (bin < 0 || bin >= m_max_bits)
-            continue;
-
-        // Update histogram
+        // Add a term to our histogram
         //
         if (intensity > 0)
-            hist[bin] += 1;
-        else
-            hist[bin] -= 1;
-        
-        //printf("%d", (int) intensity);
-    }
-    //printf("\n");
-
-    /*
-    // Display histogram
-    //
-    printf("histogram ");
-    for (int i = m_max_bits - 1; i >= 0; i--)
-        printf("%+d", (int) hist[i]);
-    printf("\n");
-   
-    // Display thresholded histogram
-    //
-    printf("threshold ");
-    for (int i = m_max_bits - 1; i >= 0; i--)
-    {
-        if (hist[i] <= m_zero_thresh)
-            printf(" 0");
-        else if (hist[i] >= m_one_thresh)
-            printf(" 1");
-        else
-            printf(" ?");
-    }
-    printf("\n");
-    */
-
-    // Assign id by thresholding the bit-histogram.
-    //
-    int id = 0;
-    for (int i = 0; i < m_max_bits; i++)
-    {
-        if (hist[i] <= m_zero_thresh)
-            id |= 0;
-        else if (hist[i] >= m_one_thresh)
-            id |= (1 << i);
-        else
         {
-            id = 0;
-            break;
+            term[term_count][0] = tcx / m_bit_width;
+            term[term_count][1] = fabs(tbx - tax) / m_bit_width;
+            term_count++;
         }
     }
 
-    //printf("id %d\n", (int) id);
+    /* for testing
+    for (double x = -0.5; x <= m_max_bits + 0.5; x += 0.1)
+    {
+        double v = 0;
+        for (int j = 0; j < term_count; j++)
+        {
+            double z = term[j][0];
+            double s = term[j][1];
+            v += exp(-(x - z) * (x - z) / (2 * s * s));
+        }
+        printf("%1.3f %1.3f\n", (double) x, (double) v);
+    }
+    printf("\n");
+    printf("\n");
+    */
 
+    int id = 0;
+    double one_thresh;
+    double zero_thresh;
+
+    // Identify bits
+    // Assumes first two bits are 1 and 0.
+    //
+    for (int bit = 0; bit < m_max_bits; bit++)
+    {
+        double x = (double) bit + 0.5;
+        double v = 0;
+        for (int j = 0; j < term_count; j++)
+        {
+            double z = term[j][0];
+            double s = term[j][1];
+            v += exp(-(x - z) * (x - z) / (2 * s * s));
+        }
+
+        if (bit == 0)
+            one_thresh = v - 0.5;
+        else if (bit == 1)
+            zero_thresh = v + 0.5;
+        
+        if (v > one_thresh)
+            id |= (1 << bit);
+        else if (v > zero_thresh)
+            return 0;
+    }
+    if (one_thresh - zero_thresh < 0)
+        return 0;
+    
     return id;
 }
 
