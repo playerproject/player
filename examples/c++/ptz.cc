3cc9b8ab25eb3979395a3ebc290a7e0d431afd91
Brian Gerkey <brian@gerkey.org>
2001-05-25 20:51:42 +0000

    new c++ examples

---
+++ examples/c++/ptz.cc
@@ -0,0 +1,95 @@
+/*
+ * ptz.cc
+ *
+ * a simple demo to show how to send commands to and get feedback from
+ * the Sony PTZ camera.  this program will pan the camera in a loop
+ * from side to side
+ */
+
+#include <playerclient.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+char host[256] = "localhost";
+int port = PLAYER_PORTNUM;
+
+/* parse command-line args */
+void
+parse_args(int argc, char** argv)
+{
+  int i;
+
+  i=1;
+  while(i<argc)
+  {
+    if(!strcmp(argv[i],"-h"))
+    {
+      if(++i<argc)
+        strcpy(host,argv[i]);
+      else
+      {
+        //puts(USAGE);
+        exit(1);
+      }
+    }
+    else if(!strcmp(argv[i],"-p"))
+    {
+      if(++i<argc)
+        port = atoi(argv[i]);
+      else
+      {
+        //puts(USAGE);
+        exit(1);
+      }
+    }
+    else
+    {
+      //puts(USAGE);
+      exit(1);
+    }
+    i++;
+  }
+}
+
+int main(int argc, char **argv)
+{ 
+  parse_args(argc,argv);
+
+  /* Connect to Player server */
+  PlayerClient robot(host,port);
+
+  /* Request sensor data */
+  PtzProxy zp(&robot,0,'a');
+  // connect to vision so that Stage will visualize the FOV
+  //VisionProxy* vp = new VisionProxy(robot,0,'r');
+
+  int dir = 1;
+  int newpan;
+  for(;;)
+  {
+    if(robot.Read())
+      exit(1);
+
+    zp.Print();
+
+    if(zp.pan > 80 || zp.pan < -80)
+    {
+      newpan = dir*70;
+      zp.SetCam(newpan,zp.tilt,zp.zoom);
+      for(int i=0;i<10;i++)
+      {
+        if(robot.Read())
+          exit(1);
+      }
+      zp.Print();
+      dir = -dir;
+    }
+    newpan = zp.pan + dir * 5;
+    zp.SetCam(newpan,zp.tilt,zp.zoom);
+  }
+
+  return(0);
+}
+
---
+++ examples/c++/sonarobstacleavoid.cc
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>  // for atoi(3)
+#include <playerclient.h>  // for player client stuff
+#include <string.h> /* for strcpy() */
+
+#define USAGE \
+  "USAGE: sonarobstacleavoid [-h <host>] [-p <port>] [-m]\n" \
+  "       -h <host>: connect to Player on this host\n" \
+  "       -p <port>: connect to Player on this TCP port\n" \
+  "       -m       : turn on motors (be CAREFUL!)"
+
+bool turnOnMotors = false;
+char host[256] = "localhost";
+int port = PLAYER_PORTNUM;
+
+/* easy little command line argument parser */
+void
+parse_args(int argc, char** argv)
+{
+  int i;
+
+  i=1;
+  while(i<argc)
+  {
+    if(!strcmp(argv[i],"-h"))
+    {
+      if(++i<argc)
+        strcpy(host,argv[i]);
+      else
+      {
+        puts(USAGE);
+        exit(1);
+      }
+    }
+    else if(!strcmp(argv[i],"-p"))
+    {
+      if(++i<argc)
+        port = atoi(argv[i]);
+      else
+      {
+        puts(USAGE);
+        exit(1);
+      }
+    }
+    else if(!strcmp(argv[i],"-m"))
+    {
+      turnOnMotors = true;
+    }
+    else
+    {
+      puts(USAGE);
+      exit(1);
+    }
+    i++;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  unsigned short min_front_dist = 500;
+  unsigned short really_min_front_dist = 300;
+  char avoid;
+
+  parse_args(argc,argv);
+
+  PlayerClient robot(host,port);
+
+  PositionProxy pp(&robot,0,'w');
+  SonarProxy sp(&robot,0,'r');
+
+  /* maybe turn on the motors */
+  if(turnOnMotors && pp.SetMotorState(1))
+    exit(1);
+
+  int newspeed, newturnrate;
+  /* go into read-think-act loop */
+  for(;;)
+  {
+    /* this blocks until new data comes; 10Hz by default */
+    if(robot.Read())
+      exit(1);
+
+    /* print current sensor data to console */
+    //sp.Print();
+
+    /*
+     * sonar avoid.
+     *   policy (pretty stupid):
+     *     if(object really close in front)
+     *       backup and turn away;
+     *     else if(object close in front)
+     *       stop and turn away;
+     */
+    avoid = 0;
+    newspeed = 200;
+
+    if (avoid == 0)
+    {
+        if((sp.ranges[2] < really_min_front_dist) ||
+           (sp.ranges[3] < really_min_front_dist) ||
+           (sp.ranges[4] < really_min_front_dist) ||
+           (sp.ranges[5] < really_min_front_dist))
+        {
+            avoid = 50;
+            newspeed = -100;
+        }
+        else if((sp.ranges[2] < min_front_dist) ||
+                (sp.ranges[3] < min_front_dist) ||
+                (sp.ranges[4] < min_front_dist) ||
+                (sp.ranges[5] < min_front_dist))
+        {
+            newspeed = 0;
+            avoid = 50;
+        }
+    }
+
+    if(avoid > 0)
+    {  
+      if((sp.ranges[0] + sp.ranges[1]) < 
+         (sp.ranges[6] + sp.ranges[7]))
+        newturnrate = 30;
+      else
+        newturnrate = -30;
+      avoid--;
+    }
+    else
+      newturnrate = 0;
+
+    /* write commands to robot */
+    pp.SetSpeed(newspeed,newturnrate);
+  }
+}
