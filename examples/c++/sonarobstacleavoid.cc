96ca501fcac932fcb47ffa97dc154a2ecefc0517
Brian Gerkey <brian@gerkey.org>
2001-10-02 00:12:56 +0000

    minor changes

--- examples/c++/sonarobstacleavoid.cc
+++ examples/c++/sonarobstacleavoid.cc
@@ -66,7 +66,7 @@ int main(int argc, char **argv)
   PlayerClient robot(host,port);
 
   PositionProxy pp(&robot,0,'a');
-  LaserProxy lp(&robot,0,'r');
+  //LaserProxy lp(&robot,0,'r');
   SonarProxy sp(&robot,0,'r');
 
   /* maybe turn on the motors */
