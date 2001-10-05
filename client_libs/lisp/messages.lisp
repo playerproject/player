9331865f94123f13ce397eeded05b98aa5be6a46
Brian Gerkey <brian@gerkey.org>
2001-10-05 19:09:10 +0000

    can read data now

--- client_libs/lisp/messages.lisp
+++ client_libs/lisp/messages.lisp
@@ -2,7 +2,22 @@
 
 ; the kind of object that we'll pass about for client control
 (defstruct playerclient
-  stream subscriptions)
+  stream subscriptions
+  misc
+  gripper
+  position
+  sonar
+  laser
+  vision
+  ptz
+  audio
+  laserbeacon
+  broadcast
+  speech
+  gps
+  occupancy
+  truth
+  bps)
 
 ; a player message header
 (defstruct player-msgheader
@@ -24,20 +39,36 @@
             access uint8))
 (setq *PLAYER-PLAYER-DEV-REQ-SIZE* 7)
 
+(defun player-make-datamode-payload (mode) 
+  (list *PLAYER-PLAYER-DATAMODE-REQ* uint16 
+         mode uint8))
+(setq *PLAYER-PLAYER-DATAMODE-REQ-SIZE* 3)
+
+(defun player-make-datareq-payload () 
+  (list *PLAYER-PLAYER-DATA-REQ* uint16))
+(setq *PLAYER-PLAYER-DATA-REQ-SIZE* 2)
+
 (defstruct player-devresp
   device
   index
   access)
-
 (defun player-devresp-format () (list uint16 uint16 uint16 uint8))
 (defun player-make-devresp (arglist)
   (make-player-devresp :device (cadr arglist) 
                        :index (caddr arglist) 
                        :access (cadddr arglist)))
 
-(defun player-sonar-data-format () 
-  (list uint16 uint16 uint16 uint16
-        uint16 uint16 uint16 uint16
-        uint16 uint16 uint16 uint16
-        uint16 uint16 uint16 uint16))
+
+(defun player-sonar-data-format (&OPTIONAL (count 0)) 
+  (cond 
+    ((= count 16) '())
+    (T (append (list uint16) (player-sonar-data-format (+ count 1))))))
+
+(defun player-laser-data-format (&OPTIONAL (count 0)) 
+  (cond 
+    ((= count 401) '())
+    ((= count 0) 
+      (append (list int16 int16 uint16 uint16 uint16)
+                 (player-laser-data-format (+ count 1))))
+    (T (append (list uint16) (player-laser-data-format (+ count 1))))))
 
