cc5fa8625ccf2eb7d2ed2db56b1b83120ef007f9
Richard Vaughan <vaughan.sourceforge..ca>
2002-03-04 20:56:01 +0000

    added multimapping example, reverted multirandom example to straightforward random walk

--- examples/c++/multimapping.cc
+++ examples/c++/multimapping.cc
@@ -30,6 +30,16 @@ int numclients;
 char host[256];
 int baseport;
 
+void sig_int(int num)
+{
+  struct timeval curr;
+  gettimeofday(&curr,NULL);
+  printf("# stopped at %s\n", ctime(&(curr.tv_sec)));
+  fprintf(stderr,"# stopped at %s\n", ctime(&(curr.tv_sec)));
+  fflush(NULL);
+  exit(0);
+}
+
 /* parse command-line args */
 void
 parse_args(int argc, char** argv)
@@ -48,6 +58,10 @@ parse_args(int argc, char** argv)
 #define STRAIGHT 0
 #define TURN 1
 
+#define IMG_WIDTH 3117
+#define IMG_HEIGHT 1189
+#define PPM 12.0
+
 int main(int argc, char** argv)
 {
   int randint;
@@ -55,10 +69,34 @@ int main(int argc, char** argv)
   bool obs = false;
   bool write = false;
 
+  char* occ_grid = new char[IMG_WIDTH*IMG_HEIGHT];
+
+  bzero(occ_grid,IMG_HEIGHT*IMG_WIDTH);
+  
+  // to flush logs
+  signal(SIGINT,sig_int);
+  signal(SIGTERM,sig_int);
+  signal(SIGHUP,sig_int);
+
+  // log header stuff
+  puts("# Occupancy grid");
+  printf("# %s running %d robots\n", host,numclients);
+  struct timeval curr;
+  gettimeofday(&curr,NULL);
+  printf("# started at %s\n", ctime(&(curr.tv_sec)));
+  puts("# format (seconds and meters):");
+  puts("# t sx sy ");
+  
+  // log header stuff
+  fputs("# Robot position log\n",stderr);
+  fprintf(stderr,"# %s running %d robots\n", host,numclients);
+  fprintf(stderr,"# started at %s\n", ctime(&(curr.tv_sec)));
+  fputs("# format (seconds and meters and degrees):\n",stderr);
+  fputs("# t r0x r0y r0th .....\n",stderr);
+
   /* first, parse command line args */
   parse_args(argc,argv);
 
-  struct timeval curr;
   // seed the RNG
   gettimeofday(&curr,NULL);
   srand(curr.tv_usec);
@@ -68,6 +106,7 @@ int main(int argc, char** argv)
   PlayerClient** clients = new PlayerClient*[numclients];
   PositionProxy** pproxies = new PositionProxy*[numclients];
   SonarProxy** sproxies = new SonarProxy*[numclients];
+  GpsProxy** gproxies = new GpsProxy*[numclients];
 
   int* randcounts = new int[numclients];
   int* avoidcounts = new int[numclients];
@@ -84,6 +123,7 @@ int main(int argc, char** argv)
 
     pproxies[i] = new PositionProxy(clients[i],0,'a');
     sproxies[i] = new SonarProxy(clients[i],0,'r');
+    gproxies[i] = new GpsProxy(clients[i],0,'r');
   }
 
   int newturnrate=0,newspeed=0;
@@ -95,6 +135,7 @@ int main(int argc, char** argv)
   if(mc.Read())
     exit(1);
 
+  int count = 0;
   for(;;)
   {
     if(mc.Read())
@@ -107,6 +148,24 @@ int main(int argc, char** argv)
       write = true;
       if(clients[i]->fresh)
       {
+        // every so often, log robot positions
+        // at 10Hz, 50 cycles is 5 sec
+        if((i == 0) && !(++count % 50))
+        {
+          fprintf(stderr,"%f ",
+                    gproxies[i]->timestamp.tv_sec + 
+                    gproxies[i]->timestamp.tv_usec / 1000000.0);
+          for(int j=0;j<numclients;j++)
+          {
+            fprintf(stderr,"%.3f %.3f %d ",
+                    gproxies[j]->xpos/1000.0,
+                    gproxies[j]->ypos/1000.0,
+                    RTOD(NORMALIZE(DTOR(gproxies[j]->heading))));
+          }
+          fputs("\n",stderr);
+          count = 0;
+        }
+
         obs = (sproxies[i]->ranges[1] < minfrontdistance ||
                sproxies[i]->ranges[2] < minfrontdistance ||
                sproxies[i]->ranges[3] < minfrontdistance ||
@@ -185,6 +244,41 @@ int main(int argc, char** argv)
                //newspeed, newturnrate, i);
         if(write)
           pproxies[i]->SetSpeed(newspeed,newturnrate);
+
+        // transform sonar hit points to global coord
+        for(int j=0;j<PLAYER_NUM_SONAR_SAMPLES;j++)
+        {
+          if(sproxies[i]->ranges[j] >= 5000)
+            continue;
+          // first to robot coord
+          // find where the sonar is
+          double px,py,pth;
+          sproxies[i]->GetSonarPose(j,&px,&py,&pth);
+          double rx = px + (sproxies[i]->ranges[j]/1000.0)*cos(pth);
+          double ry = py + (sproxies[i]->ranges[j]/1000.0)*sin(pth);
+
+          //gproxies[i]->Print();
+          // now to global
+          double gx = gproxies[i]->xpos/1000.0 + 
+                  rx*cos(DTOR(gproxies[i]->heading)) - 
+                  ry*sin(DTOR(gproxies[i]->heading));
+          double gy = gproxies[i]->ypos/1000.0 +
+                  rx*sin(DTOR(gproxies[i]->heading)) +
+                  ry*cos(DTOR(gproxies[i]->heading));
+
+          int occ_x = (int)rint(gx * 12.0);
+          int occ_y = (int)rint(gy * 12.0);
+
+          if(!occ_grid[occ_x+occ_y*IMG_WIDTH])
+          {
+            occ_grid[occ_x+occ_y*IMG_WIDTH] = 1;
+
+            printf("%f %f %f\n", 
+                   sproxies[i]->timestamp.tv_sec + 
+                   sproxies[i]->timestamp.tv_usec / 1000000.0,
+                   gx, gy);
+          }
+        }
 
         clients[i]->fresh = false;
       }
