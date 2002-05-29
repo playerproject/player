1284514b3fa971783792ba02d7d2788bacc6ca72
Brian Gerkey <brian@gerkey.org>
2002-05-29 04:13:56 +0000

    added gripper test

---
+++ client_libs/c++/test/test_gripper.cc
@@ -0,0 +1,86 @@
+/*
+ * $Id$
+ *
+ * a test for the C++ SonarProxy
+ */
+
+#include "playerclient.h"
+#include "test.h"
+#include <unistd.h>
+
+int
+test_gripper(PlayerClient* client, int index)
+{
+  unsigned char access;
+  GripperProxy gp(client,index);
+
+  printf("device [gripper] index [%d]\n", index);
+
+  TEST("subscribing (read/write)");
+  if((gp.ChangeAccess(PLAYER_ALL_MODE,&access) < 0) ||
+     (access != PLAYER_ALL_MODE))
+  {
+    FAIL();
+    return -1;
+  }
+  PASS();
+
+  // wait for P2OS to start up
+  for(int i=0; i < 20; i++)
+    client->Read();
+
+  for(int t = 0; t < 3; t++)
+  {
+    TEST1("reading data (attempt %d)", t);
+
+    if(client->Read() < 0)
+    {
+      FAIL();
+      return(-1);
+    }
+
+    PASS();
+
+    gp.Print();
+  }
+
+  TEST("lift down");
+  if(gp.SetGrip(LIFTdown) < 0)
+  {
+    FAIL();
+    return(-1);
+  }
+  sleep(3);
+  PASS();
+
+  TEST("paddles open");
+  if(gp.SetGrip(GRIPopen) < 0)
+  {
+    FAIL();
+    return(-1);
+  }
+  sleep(3);
+  PASS();
+
+  TEST("gripper store");
+  if(gp.SetGrip(GRIPstore) < 0)
+  {
+    FAIL();
+    return(-1);
+  }
+  sleep(4);
+  PASS();
+
+  TEST("unsubscribing");
+  if((gp.ChangeAccess(PLAYER_CLOSE_MODE,&access) < 0) ||
+     (access != PLAYER_CLOSE_MODE))
+  {
+    FAIL();
+    return -1;
+  }
+
+  PASS();
+
+  return(0);
+}
+
