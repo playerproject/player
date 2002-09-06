1aa756141975214fb45fdf93041fa9920fee42de
John Sweeney <jazzfunk@users.sourceforge.net>
2002-09-06 18:35:34 +0000

    Many changes here.  From most general to least: - Changed the configure scripts to allow selectively compiling devices in the player server, as well as being able to specify compiling the server for cross compilation using the LART ARM cross compiler.  This affects configure.in and all the Makefile.am's under server/.  Note that if you run "./bootstrap" it will comple things exactly as before (all the preexsiting devices default to being compiled).  However, for example, if you don't want to compile the SICK device, you'd run "./bootstrap --without-sick".  The stage device is always compiled though.
    To compile for the ARM, run "./bootstrap --with-server-arch=arm."
    
    - Modified the player server to remove almost all floating point math, to make it more friendly on the ARM.  All the ioctls remain the same, so you shouldn't notice anything different.
    
    - Added a WiFi (aka 802.11 aka wireless ethernet) device.  Currently read-only, and it assumes the card is on eth0.  It just reads the quality of the connection using /proc/net/wireless.  More work to be done here for configuration, etc.   This device will not compile by default, so use "./bootstrap --with-wifi" to compile.
    
    - Added drivers for the K-Team Kameleon 376SBC with Robotics Extension Board (REB) under drivers/reb.  This includes a position, IR, and power device for the REB.

---
+++ server/drivers/reb/reb_position.cc
@@ -0,0 +1,128 @@
+/*
+ *  Player - One Hell of a Robot Server
+ *  Copyright (C) 2000  
+ *     Brian Gerkey, Kasper Stoy, Richard Vaughan, & Andrew Howard
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+/* Copyright (C) 2002
+ *   John Sweeney, UMASS, Amherst, Laboratory for Perceptual Robotics
+ *
+ * $Id$
+ * 
+ * the REB position device implementation.  basically copied from
+ * positiondevice.cc
+ *
+ */
+
+#include <reb.h>
+#include <stdio.h>
+#include <string.h>
+
+class REBPosition: public REB
+{
+public:
+  // constructor just calls REBDevice constructor
+  REBPosition(char *interface, ConfigFile *cf, int section) : 
+    REB(interface, cf, section) {}
+  ~REBPosition();
+  
+  virtual size_t GetData(unsigned char *, size_t maxsize,
+			 uint32_t *timestamp_sec, 
+			 uint32_t *timestamp_usec);
+
+  void PutCommand(unsigned char *, size_t maxsize);
+};
+
+CDevice * REBPosition_Init(char *interface, ConfigFile *cf, int section);
+void REBPosition_Register(DriverTable *table);
+
+/* Global initialization function.  Checks whether specified interface
+ * is supported by the device.
+ *
+ * returns: pointer to new REBPosition object
+ */
+CDevice *
+REBPosition_Init(char *interface, ConfigFile *cf, int section)
+{
+  if (!strcmp(interface, PLAYER_POSITION_STRING) 
+      || !strcmp(interface, PLAYER_REB_POSITION_STRING)) {
+    return ( (CDevice *) new REBPosition(interface, cf, section));
+  } else {
+    PLAYER_ERROR1("driver \"reb_position\" does not support interface \"%s\"\n",
+		  interface);
+    return NULL;
+  }
+}
+
+/* registers the driver with the drivertable
+ *
+ * returns: 
+ */
+void
+REBPosition_Register(DriverTable *table) 
+{
+  table->AddDriver("reb_position", PLAYER_ALL_MODE, REBPosition_Init);
+}
+
+/* destructor.  just set commanded speed to nil
+ *
+ * returns:
+ */
+REBPosition::~REBPosition()
+{
+  ((player_reb_cmd_t *)device_command)->position.xspeed = 0;
+  ((player_reb_cmd_t *)device_command)->position.yawspeed = 0;
+  ((player_reb_cmd_t *)device_command)->position.yaw = 0;
+}
+
+/* updates data to go to client
+ *
+ * returns: size of data copied
+ */
+size_t
+REBPosition::GetData(unsigned char *dest, size_t maxsize,
+			    uint32_t *timestamp_sec, uint32_t *timestamp_usec)
+{
+  Lock();
+
+  *((player_position_data_t*)dest) = 
+    ((player_reb_data_t *)device_data)->position;
+  *timestamp_sec = data_timestamp_sec;
+  *timestamp_usec = data_timestamp_usec;
+  
+  Unlock();
+
+  return sizeof(player_position_data_t);
+}
+
+/* copies a command into the device's buffer
+ *
+ * returns:
+ */
+void
+REBPosition::PutCommand(unsigned char *src, size_t size)
+{
+  if (size != sizeof(player_position_cmd_t) ) {
+    fprintf(stderr, "REBPosition: PutCommand(): command wrong size. ignoring (%d)/%d\n", size,
+	    sizeof(player_position_cmd_t));
+  } else {
+    ((player_reb_cmd_t *)device_command)->position = 
+      *((player_position_cmd_t *)src);
+  }
+}
---
+++ server/drivers/wifi.cc
@@ -0,0 +1,178 @@
+/*
+ *  Player - One Hell of a Robot Server
+ *  Copyright (C) 2000  
+ *     Brian Gerkey, Kasper Stoy, Richard Vaughan, & Andrew Howard
+ *                      
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+/* Copyright (C) 2002
+ *   John Sweeney, UMASS, Amherst, Laboratory for Perceptual Robotics
+ *
+ * $Id$
+ *
+ * WiFi driver.  Reads the wireless info found in /proc/net/wireless.
+ * sort of ad hoc right now, as I've only tested on our own orinoco
+ * cards.  
+ *
+ * This will only update every WIFI_UPDATE_PERIOD milliseconds
+ * 
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <device.h>
+#include <configfile.h>
+#include <playertime.h>
+#include <drivertable.h>
+#include <player.h>
+
+extern PlayerTime *GlobalTime;
+
+#define WIFI_UPDATE_PERIOD 2000
+#define WIFI_INFO_FILE "/proc/net/wireless"
+
+class WiFi : public CDevice
+{
+public:
+  WiFi(char *interface, ConfigFile *cf, int section) :
+    CDevice(0, 0, 0, 1) {}
+
+  //  virtual void Main();
+
+  virtual size_t GetData(unsigned char *, size_t maxsize,
+			 uint32_t *timestamp_sec,
+			 uint32_t *timestamp_usec);
+
+  virtual int Setup();
+  virtual int Shutdown();
+
+protected:
+  FILE *info_fp; // pointer to wifi info file
+  fpos_t start_pos; // position of relevant info in info file
+
+};
+
+CDevice * WiFi_Init(char *interface, ConfigFile *cf, int section);
+void WiFi_Register(DriverTable *table);
+
+/* check for supported interfaces.
+ *
+ * returns: pointer to new WiFi driver if supported, NULL else
+ */
+CDevice *
+WiFi_Init(char *interface, ConfigFile *cf, int section)
+{ 
+  if(strcmp(interface, PLAYER_WIFI_STRING)) {
+    PLAYER_ERROR1("driver \"wifi\" does not support interface \"%s\"\n",
+		  interface);
+    return NULL;
+  } else {
+    return ((CDevice*)(new WiFi(interface, cf, section)));
+  }
+}
+
+/* register with drivertable
+ *
+ * returns: 
+ */
+void
+WiFi_Register(DriverTable *table)
+{
+  table->AddDriver("wifi", PLAYER_READ_MODE, WiFi_Init);
+}
+
+int
+WiFi::Setup()
+{
+  if ((this->info_fp = fopen(WIFI_INFO_FILE, "r")) == NULL) {
+    fprintf(stderr, "WiFi: couldn't open info file \"%s\"\n",
+	    WIFI_INFO_FILE);
+    return -1;
+  } 
+  
+  // lets read it to the point we are interested in
+  char buf[128];
+  // read 3 lines
+  for (int i =0; i < 3; i++) {
+    if (i == 2) {
+      fgetpos(this->info_fp, &this->start_pos);
+    }
+    
+    if (fgets(buf, sizeof(buf),this->info_fp) == NULL) {
+      fprintf(stderr, "WiFi: couldn't read line from info file\n");
+      fclose(this->info_fp);
+      return -1;
+    }
+  }
+  
+  // now we are at line of interest
+  int eth, status, link, level, noise;
+  sscanf(buf, "  eth%d: %d %d %d %d", &eth, &status,
+	 &link, &level, &noise);
+  
+  return 0;
+}
+
+int
+WiFi::Shutdown()
+{
+  fclose(this->info_fp);
+
+  return 0;
+}
+
+/* main loop.  just read info from file, update data, then sleep
+ *
+ * returns: 
+ */
+size_t
+WiFi::GetData(unsigned char *dest, size_t maxsize, uint32_t *timestamp_sec,
+	      uint32_t *timestamp_usec)
+{
+  player_wifi_data_t data;
+  int eth, status, link, level, noise;
+  unsigned short wlink, wlevel, wnoise;
+
+  struct timeval curr;
+  
+  // get the wifi info
+  if (fsetpos(this->info_fp, &this->start_pos)) {
+    fprintf(stderr, "WiFi: fsetpos returned error\n");
+  }
+  
+  fscanf(this->info_fp, "  eth%d: %d %d %d %d", &eth, &status,
+	 &link, &level, &noise);
+  
+  wlink = (unsigned short)link;
+  wlevel = (unsigned short) level;
+  wnoise = (unsigned short) noise;
+  data.link = htons(wlink);
+  data.level = htons(wlevel);
+  data.noise = htons(wnoise);
+
+  assert(sizeof(data) < maxsize);
+  memcpy(dest, &data, sizeof(data));
+
+  GlobalTime->GetTime(&curr);
+  *timestamp_sec = curr.tv_sec;
+  *timestamp_usec = curr.tv_usec;
+
+  return (sizeof(data));
+}
