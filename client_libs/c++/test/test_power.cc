99ca03a447dd4ecd2a5a674c016e9a8256d951d3
Richard Vaughan <vaughan@sfu.ca>
2002-10-26 00:54:46 +0000

    added power proxy test

---
+++ client_libs/c++/test/test_power.cc
@@ -0,0 +1,57 @@
+/*
+ * $Id$
+ *
+ * a test for the C++ PositionProxy
+ */
+
+#include "playerclient.h"
+#include "test.h"
+#include <unistd.h>
+
+int
+test_power(PlayerClient* client, int index)
+{
+  unsigned char access;
+  PowerProxy pp(client,index);
+
+  printf("device [power] index [%d]\n", index);
+
+  TEST("subscribing (read/write)");
+  if((pp.ChangeAccess(PLAYER_ALL_MODE,&access) < 0) ||
+     (access != PLAYER_ALL_MODE))
+  {
+    FAIL();
+    printf("DRIVER: %s\n", pp.driver_name);
+    return -1;
+  }
+  PASS();
+  printf("DRIVER: %s\n", pp.driver_name);
+
+  for(int t = 0; t < 3; t++)
+  {
+    TEST1("reading data (attempt %d)", t);
+
+    if(client->Read() < 0)
+    {
+      FAIL();
+      return(-1);
+    }
+
+    PASS();
+
+    pp.Print();
+  }
+
+  TEST("unsubscribing");
+  if((pp.ChangeAccess(PLAYER_CLOSE_MODE,&access) < 0) ||
+     (access != PLAYER_CLOSE_MODE))
+  {
+    FAIL();
+    return -1;
+  }
+
+  PASS();
+
+  return(0);
+}
+
