6ee1bcd5f34f0cd9a318166a5d3411444b48d528
Andrew Howard <ahoward@usc.edu>
2002-11-27 22:08:58 +0000

    *** empty log message ***

--- server/drivers/iwspy.cc
+++ server/drivers/iwspy.cc
@@ -36,8 +36,10 @@
 #include <stdlib.h>
 
 #include <netinet/in.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 #include <device.h>
@@ -70,6 +72,9 @@ class Iwspy : public CDevice
   // Parse the iwspy output
   private: void Parse(int fd);
 
+  // Lookup a MAC address
+  private: int ArpLookup(const char *ip, char *mac);
+
   // Start pinging
   private: int StartPing();
 
@@ -79,11 +84,17 @@ class Iwspy : public CDevice
   // Data for each NIC to be monitored
   private: struct nic_t
   {
+    // IP address of the NIC
+    char ip[64];
+    
     // MAC address of NIC
-    const char *mac;
+    char mac[64];
 
     // Link properties
     int link, level, noise;
+
+    // Counters to keep track of new/old information.
+    int in_count, out_count;
   };
 
   // The list of NIC's to be monitored
@@ -124,12 +135,27 @@ void Iwspy_Register(DriverTable *table)
 ////////////////////////////////////////////////////////////////////////////////
 // Constructor
 Iwspy::Iwspy(char *interface, ConfigFile *cf, int section)
-    : CDevice(0, 0, 0, 1)
+    : CDevice(sizeof(player_wifi_data_t), 0, 0, 0)
 {
-  // TODO: read from config file
+  int i;
+  char key[64];
+  const char *ip, *mac;
+
+  // Read IP addresses to monitor
   this->nic_count = 0;
-  this->nics[this->nic_count++].mac = "00:30:AB:15:3D:D7";
-  
+  for (i = 0; i < 8; i++)
+  {
+    snprintf(key, sizeof(key), "nic_%d", i);
+    ip = cf->ReadTupleString(section, key, 0, NULL);
+    mac = cf->ReadTupleString(section, key, 1, NULL);
+
+    if (!ip || !mac)
+      break;
+
+    strcpy(this->nics[this->nic_count].ip, ip);
+    strcpy(this->nics[this->nic_count].mac, mac);
+    this->nic_count++;
+  }
   return;
 }
 
@@ -138,17 +164,35 @@ Iwspy::Iwspy(char *interface, ConfigFile *cf, int section)
 // Initialize driver
 int Iwspy::Setup()
 {
-  // Start the device thread
-  StartThread();
-
-  // Initialize the watch list
-  if (this->InitIwSpy() != 0)
-    return -1;
+  /* REMOVE?
+  int i;
+  char mac[64];
+  */
 
   // Start pinging
   if (this->StartPing() != 0)
     return -1;
+
+  /* REMOVE?
+  // Wait a while until the arp table is refreshed
+  usleep(1000000);
   
+  // Get the mac addresses
+  for (i = 0; i < this->nic_count; i++)
+  {
+    if (this->ArpLookup(this->nics[i].ip, mac) != 0)
+      return -1;
+    strcpy(this->nics[i].mac, mac);
+  }
+  */
+  
+  // Initialize the watch list
+  if (this->InitIwSpy() != 0)
+    return -1;
+
+  // Start the device thread
+  StartThread();
+
   return 0;
 }
 
@@ -170,21 +214,46 @@ int Iwspy::Shutdown()
 ////////////////////////////////////////////////////////////////////////////////
 // Main function for device thread
 void Iwspy::Main() 
-{
+{ 
+  int i;
+  nic_t *nic;
+  struct timeval time;
+  player_wifi_link_t *link;
+  player_wifi_data_t data;
+  
   while (true)
   {
     // Test if we are supposed to terminate.
     pthread_testcancel();
     usleep(100000);
 
-    // Ping each of the IP's on our watch list.
-    // TODO
+    // Get the time at which we started reading.
+    // This is not a great estimate of when the phenomena occurred.
+    GlobalTime->GetTime(&time);
 
     // Get the updated iwspy info
     this->UpdateIwSpy();
 
-    // Output the new data
-    // TODO
+    // Construct data packet
+    data.link_count = 0;
+    for (i = 0; i < this->nic_count; i++)
+    {
+      nic = this->nics + i;
+
+      if (nic->in_count > nic->out_count)
+      {
+        link = data.links + data.link_count++;
+        strcpy(link->ip, nic->ip);
+        link->link = htons(nic->link);
+        link->level = htons(nic->level);
+        link->noise = htons(nic->noise);
+        nic->out_count = nic->in_count;
+      }
+    }
+    data.link_count = htons(data.link_count);
+
+    // Send data
+    PutData((uint8_t*) &data, sizeof(data), time.tv_sec, time.tv_usec);
   }
   return;
 }
@@ -212,7 +281,7 @@ int Iwspy::InitIwSpy()
 
     // Add the list of MAC addresses to be monitored.
     for (i = 0; i < this->nic_count; i++)
-      args[argc++] = (char*) this->nics[i].mac;
+      args[argc++] = this->nics[i].mac;
     
     args[argc++] = NULL;
     
@@ -305,6 +374,8 @@ void Iwspy::Parse(int fd)
   char line[1024];
   char mac[16];
   int link, level, noise;
+  char status[16];
+  nic_t *nic;
 
   bytes = read(fd, buffer, sizeof(buffer));
   if (bytes < 0)
@@ -313,7 +384,7 @@ void Iwspy::Parse(int fd)
     return;
   }
 
-  printf("%s\n", buffer);
+  //printf("%s\n", buffer);
 
   for (i = 0; i < bytes;)
   {
@@ -325,11 +396,25 @@ void Iwspy::Parse(int fd)
     
     //printf("[%s]\n", line);
 
-    if (sscanf(line, " %s : Quality:%d/%*d Signal level:%d dBm  Noise level:%d dBm",
-               mac, &link, &level, &noise) < 4)
+    if (sscanf(line, " %s : Quality:%d/%*d Signal level:%d dBm  Noise level:%d dBm (%s)",
+               mac, &link, &level, &noise, status) < 5)
       continue;
 
-    printf("iwspy: %s %d %d %d\n", mac, link, level, noise);
+    // Update the appropriate entry in the nic list.
+    for (j = 0; j < this->nic_count; j++)
+    {
+      nic = this->nics + j;
+
+      if (strcmp(nic->mac, mac) == 0)
+      {
+        nic->link = link;
+        nic->level = level;
+        nic->noise = noise;
+        nic->in_count++;
+        printf("iwspy: %s %d %d %d\n", nic->ip, link, level, noise);
+        break;
+      }
+    }
   }
 
   return;
@@ -337,17 +422,96 @@ void Iwspy::Parse(int fd)
 
 
 ////////////////////////////////////////////////////////////////////////////////
+// Lookup a MAC address
+int Iwspy::ArpLookup(const char *ip, char *mac)
+{
+  int bytes;
+  char line[1024];
+  int status;
+  pid_t pid;
+  int stdout_pipe[2];
+
+  // Create pipes
+  if (pipe(stdout_pipe) < 0)
+  {
+    PLAYER_ERROR1("error on pipe: [%s]", strerror(errno));
+    return -1;
+  }
+
+  // Fork here
+  pid = fork();
+
+  // If in the child process...
+  if (pid == 0)
+  {
+    close(1);
+    dup(stdout_pipe[1]);
+    close(stdout_pipe[0]);
+    close(stdout_pipe[1]);
+
+    // Run iwspy
+    if (execl("/sbin/arp", "arp", "-n", "-a", ip, NULL) != 0)
+    {
+      PLAYER_ERROR1("error on exec: [%s]", strerror(errno));
+      exit(errno);
+    }
+    assert(false);
+  }
+
+  // If in the parent process...
+  else
+  {
+    // Wait for the child to finish
+    if (waitpid(pid, &status, 0) < 0)
+    {
+      PLAYER_ERROR1("error on waitpid: [%s]", strerror(errno));
+      return -1;
+    }
+
+    // Parse the output
+    bytes = read(stdout_pipe[0], line, sizeof(line));
+    if (bytes < 0)
+    {
+      PLAYER_ERROR1("error reading data: [%s]", strerror(errno));
+      return -1;
+    }
+
+    //printf("arp [%s]\n", line);
+
+    if (sscanf(line, "%*s %*s at %s ", mac) < 1)
+    {
+      PLAYER_ERROR1("unable to get hardware address for [%s]", ip);
+      return -1;
+    }
+    
+    close(stdout_pipe[0]);
+    close(stdout_pipe[1]);
+  }
+  
+  return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 // Start ping
 int Iwspy::StartPing()
 {
+  int dummy_fd;
+  
   // Fork here
   this->ping_pid = fork();
 
   // If in the child process...
   if (this->ping_pid == 0)
   {
+    // Pipe all the output to /dev/null
+    dummy_fd = open("/dev/null", O_RDWR);
+    dup2(dummy_fd,0);
+    dup2(dummy_fd,1);
+    dup2(dummy_fd,2);
+
     // Run ping
-    if (execl("/bin/ping", "ping", "10.0.1.254", NULL) != 0)
+    if (execl("/bin/ping", "ping", "-b", "10.0.0.0", NULL) != 0)
     {
       PLAYER_ERROR1("error on exec: [%s]", strerror(errno));
       exit(errno);
