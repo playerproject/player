1fcffdcf44c85741f7c24d8c95ee09f86279e5c4
John Sweeney <jazzfunk@users.sourceforge.net>
2002-12-10 05:34:27 +0000

    backlog of updates.  fixed precision of odometry (now keeping track of sub-milli position)...  sped things up with LPRSerCom...

--- server/drivers/mixed/reb/reb_params.cc
+++ server/drivers/mixed/reb/reb_params.cc
@@ -19,11 +19,15 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-
-/* 
+/*
+ * Copyright (C) 2002
+ * John Sweeney, Laboratory for Perceptual Robotics, UMASS, Amherst
+ *
+ * $Id$
+ * 
  * reb_params.cc
  *
- * Parameters for the UMass UBot
+ * Parameters for the UMass UBots
 */
 #include <reb_params.h>
 
@@ -35,20 +39,23 @@ UBotRobotParams_t ubot_slow_params =
   150, // MAX Rot Velocity
   90, // radius in mm
   139, // axle length in mm
-  8, // numberof IR
-  100, // time between readings in ms
   // the following is # of pulese per mm
   // derived as (pulses in one rev)/(wheel diameter*pi)
   // for the slow ubot the gear reduction is 4*43*16*2:1 = 5504:1
   // the wheel diam is 72 mm
   24.333022, // pulses per mm
-  24333022, // previous * 1e6 (REB_FIXED_FACTOR)
-  0.041096416, // inverse of previous= mm per pulse
-  41096,
-  0.24333022, // this is pulses/mm * 0.01 s -> pulses/mm 10 ms
+  //  24333022, // previous * (REB_FIXED_FACTOR)
   243330,
-  0.0004109641, // inverse of previous
+  0.041096416, // inverse of previous= mm per pulse
+  //  41096,
   411,
+  0.24333022, // this is pulses/mm * 0.01 s -> pulses/mm 10 ms
+  //  243330,
+  2433,
+  0.0004109641, // inverse of previous
+  //  411,
+  4,
+  8, // 8 IRs
   {
     {35, 0, 0},
     {25, 25, 45},
@@ -58,7 +65,48 @@ UBotRobotParams_t ubot_slow_params =
     {-25, -25, 225},
     {0, -35, 270},
     {25, -25, 315},
-  }
+  },
+  100,0,10 // position mode kp,ki,kd
+};
+
+UBotRobotParams_t ubot_fast_params = 
+{
+  "UBot",  //Class
+  "fast", //Subclass
+  700, //Max Trans Velocity
+  150, // MAX Rot Velocity
+  90, // radius in mm
+  139, // axle length in mm
+  // the following is # of pulese per mm
+  // derived as (pulses in one rev)/(wheel diameter*pi)
+  // for fast ubot: 4 (quad encoding) * 14 (23/1 series has 14:1 reduction) *
+  // 16 (HE encoder has 16 pulse/rev) * 2 (reduction from gear to wheel) =
+  // 4*14*16*2 = 1792 : 1
+  // the wheel diam is 72 mm
+  7.9223794,
+  //  7922379,
+  79224,
+  0.1262247,
+  //  126225,
+  1262,
+  0.079223794,
+  //  79224,
+  792,
+  0.001262247,
+  //  1262,
+  13,
+  8, // number of IRs
+  {
+    {35, 0, 0},
+    {25, 25, 45},
+    {0, 35, 90},
+    {-25, 25, 135},
+    {-35, 0, 180},
+    {-25, -25, 225},
+    {0, -35, 270},
+    {25, -25, 315},
+  },
+  1200,0,10 //positoin mode kp, ki, kd
 };
 
 
@@ -69,4 +117,5 @@ void
 initialize_reb_params(void)
 {
   PlayerUBotRobotParams[0] = ubot_slow_params;
+  PlayerUBotRobotParams[1] = ubot_fast_params;
 }
