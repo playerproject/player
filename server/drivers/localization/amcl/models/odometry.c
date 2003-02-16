8d2b79e66a6a3c1685378236fb54260e6234a3b6
Andrew Howard <ahoward@usc.edu>
2003-02-16 23:01:41 +0000

    Fixed memory leak in odometry model

--- server/drivers/localization/amcl/models/odometry.c
+++ server/drivers/localization/amcl/models/odometry.c
@@ -13,65 +13,63 @@
 
 
 // Build a list of all empty cells in c-space
-void odometry_init_cspace(odometry_t *sensor);
+void odometry_init_cspace(odometry_t *self);
 
 
 // Create an sensor model
 odometry_t *odometry_alloc(map_t *map, double robot_radius)
 {
-  odometry_t *sensor;
+  odometry_t *self;
 
-  sensor = calloc(1, sizeof(odometry_t));
+  self = calloc(1, sizeof(odometry_t));
 
-  sensor->map = map;
-  sensor->robot_radius = robot_radius;
+  self->map = map;
+  self->robot_radius = robot_radius;
   
-  sensor->stall = 0;
+  self->init_gpdf = NULL;
+  self->init_dpdf = NULL;
+  self->action_pdf = NULL;
 
-  sensor->init_gpdf = NULL;
-  sensor->init_dpdf = NULL;
-  sensor->action_pdf = NULL;
-
-  odometry_init_cspace(sensor);
+  odometry_init_cspace(self);
   
-  return sensor;
+  return self;
 }
 
 
 // Free an sensor model
-void odometry_free(odometry_t *sensor)
+void odometry_free(odometry_t *self)
 {
-  free(sensor->ccells);
-  free(sensor);
+  free(self->ccells);
+  free(self);
   return;
 }
 
 
 // Build a list of all empty cells in c-space
-void odometry_init_cspace(odometry_t *sensor)
+void odometry_init_cspace(odometry_t *self)
 {
   int i, j;
   map_cell_t *cell;
   pf_vector_t *ccell;
 
-  sensor->ccell_count = 0;
-  sensor->ccells = malloc(sensor->map->size_x * sensor->map->size_y * sizeof(sensor->ccells[0]));
+  self->ccell_count = 0;
+  self->ccells = malloc(self->map->size_x * self->map->size_y * sizeof(self->ccells[0]));
     
-  for (j = 0; j < sensor->map->size_y; j++)
+  for (j = 0; j < self->map->size_y; j++)
   {
-    for (i = 0; i < sensor->map->size_x; i++)
+    for (i = 0; i < self->map->size_x; i++)
     {
-      cell = sensor->map->cells + MAP_INDEX(sensor->map, i, j);
+      cell = self->map->cells + MAP_INDEX(self->map, i, j);
 
       if (cell->occ_state != -1)
         continue;
-      if (cell->occ_dist < sensor->robot_radius)
+      if (cell->occ_dist < self->robot_radius)
         continue;
 
-      ccell = sensor->ccells + sensor->ccell_count++;
+      ccell = self->ccells + self->ccell_count++;
 
-      ccell->v[0] = MAP_WXGX(sensor->map, i);
-      ccell->v[1] = MAP_WYGY(sensor->map, j);
+      ccell->v[0] = MAP_WXGX(self->map, i);
+      ccell->v[1] = MAP_WYGY(self->map, j);
       ccell->v[2] = 0.0;
     }
   }
@@ -79,42 +77,42 @@ void odometry_init_cspace(odometry_t *sensor)
   return;
 }
 
-
-// Set the initial pose
-void odometry_init_pose(odometry_t *sensor, pf_vector_t pose, pf_matrix_t pose_cov)
+// Prepare to initialize the distribution; pose is the robot's initial
+// pose estimate.
+void odometry_init_init(odometry_t *self, pf_vector_t pose, pf_matrix_t pose_cov)
 {
   int i;
   pf_pdf_gaussian_t *gpdf;
   double *weights;
   pf_vector_t *ccell;
 
-  if (sensor->init_gpdf)
+  if (self->init_gpdf)
   {
-    pf_pdf_gaussian_free(sensor->init_gpdf);
-    sensor->init_gpdf = NULL;
+    pf_pdf_gaussian_free(self->init_gpdf);
+    self->init_gpdf = NULL;
   }
 
-  if (sensor->init_dpdf)
+  if (self->init_dpdf)
   {
-    pf_pdf_discrete_free(sensor->init_dpdf);
-    sensor->init_dpdf = NULL;
+    pf_pdf_discrete_free(self->init_dpdf);
+    self->init_dpdf = NULL;
   }
   
   // Create an array to put weights in
-  weights = malloc(sensor->ccell_count * sizeof(weights[0]));
+  weights = malloc(self->ccell_count * sizeof(weights[0]));
 
   // Create temporary gaussian pdf 
-  sensor->init_gpdf = pf_pdf_gaussian_alloc(pose, pose_cov);
+  self->init_gpdf = pf_pdf_gaussian_alloc(pose, pose_cov);
 
   // Determine the weight for each free cell, based on the gaussian pdf
-  for (i = 0; i < sensor->ccell_count; i++)
+  for (i = 0; i < self->ccell_count; i++)
   {
-    ccell = sensor->ccells + i;
-    weights[i] = pf_pdf_gaussian_value(sensor->init_gpdf, *ccell);
+    ccell = self->ccells + i;
+    weights[i] = pf_pdf_gaussian_value(self->init_gpdf, *ccell);
   }
     
   // Create a discrete pdf
-  sensor->init_dpdf = pf_pdf_discrete_alloc(sensor->ccell_count, weights);
+  self->init_dpdf = pf_pdf_discrete_alloc(self->ccell_count, weights);
 
   // Free temp stuff
   free(weights);
@@ -123,8 +121,20 @@ void odometry_init_pose(odometry_t *sensor, pf_vector_t pose, pf_matrix_t pose_c
 }
 
 
+// Finish initializing the distribution
+void odometry_init_term(odometry_t *self)
+{
+  pf_pdf_gaussian_free(self->init_gpdf);
+  pf_pdf_discrete_free(self->init_dpdf);
+
+  self->init_gpdf = NULL;
+  self->init_dpdf = NULL;
+  return;
+}
+
+
 // The initialization model function
-pf_vector_t odometry_init_model(odometry_t *sensor)
+pf_vector_t odometry_init_model(odometry_t *self)
 {
   int i;
   pf_vector_t pose, npose;
@@ -132,14 +142,14 @@ pf_vector_t odometry_init_model(odometry_t *sensor)
   pose = pf_vector_zero();
 
   // Guess a pose from the discrete distribution
-  i = pf_pdf_discrete_sample(sensor->init_dpdf);
-  assert(i >= 0 && i < sensor->ccell_count);
-  pose = sensor->ccells[i];
+  i = pf_pdf_discrete_sample(self->init_dpdf);
+  assert(i >= 0 && i < self->ccell_count);
+  pose = self->ccells[i];
 
   // Perturb with an orientation drawn from the gaussian distribution
-  npose = pf_pdf_gaussian_sample(sensor->init_gpdf);
-  pose.v[0] += (0.5 - (double) rand() / RAND_MAX) * sensor->map->scale;
-  pose.v[1] += (0.5 - (double) rand() / RAND_MAX) * sensor->map->scale;
+  npose = pf_pdf_gaussian_sample(self->init_gpdf);
+  pose.v[0] += (0.5 - (double) rand() / RAND_MAX) * self->map->scale;
+  pose.v[1] += (0.5 - (double) rand() / RAND_MAX) * self->map->scale;
   pose.v[2] += npose.v[2];  
 
   //printf("%f %f %f\n", pose.v[0], pose.v[1], pose.v[2]);
@@ -148,19 +158,13 @@ pf_vector_t odometry_init_model(odometry_t *sensor)
 }
 
 
-// Set the new odometric pose
-void odometry_set_pose(odometry_t *sensor, pf_vector_t old_pose, pf_vector_t new_pose)
+// Prepare to update the distribution using the action model.
+void odometry_action_init(odometry_t *self, pf_vector_t old_pose, pf_vector_t new_pose)
 {
   pf_vector_t x;
   pf_matrix_t cx;
   double ux, uy, ua;
   
-  if (sensor->action_pdf)
-  {
-    pf_pdf_gaussian_free(sensor->action_pdf);
-    sensor->action_pdf = NULL;
-  }
-
   x = pf_vector_coord_sub(new_pose, old_pose);
 
   // HACK - FIX
@@ -176,59 +180,64 @@ void odometry_set_pose(odometry_t *sensor, pf_vector_t old_pose, pf_vector_t new
   //printf("x = %f %f %f\n", x.v[0], x.v[1], x.v[2]);
   
   // Create a pdf with suitable characterisitics
-  sensor->action_pdf = pf_pdf_gaussian_alloc(x, cx); 
+  self->action_pdf = pf_pdf_gaussian_alloc(x, cx); 
 
   return;
 }
 
 
-// Set the stall bit
-void odometry_set_stall(odometry_t *sensor, int stall)
+// Finish updating the distrubiotn using the action model
+void odometry_action_term(odometry_t *self)
 {
-  sensor->stall = stall;
+  pf_pdf_gaussian_free(self->action_pdf);
+  self->action_pdf = NULL;
   return;
 }
 
 
 // The action model function
-pf_vector_t odometry_action_model(odometry_t *sensor, pf_vector_t pose)
+pf_vector_t odometry_action_model(odometry_t *self, pf_vector_t pose)
 {
   pf_vector_t z, npose;
   
-  z = pf_pdf_gaussian_sample(sensor->action_pdf);
+  z = pf_pdf_gaussian_sample(self->action_pdf);
   npose = pf_vector_coord_add(z, pose);
     
   return npose; 
 }
 
 
+// Prepare to update the distribution using the sensor model.
+void odometry_sensor_init(odometry_t *self)
+{
+  return;
+}
+
+
+// Finish updating the distribution using the sensor model.
+void odometry_sensor_term(odometry_t *self)
+{
+  return;
+}
+
+
 // The sensor model function
-double odometry_sensor_model(odometry_t *sensor, pf_vector_t pose)
+double odometry_sensor_model(odometry_t *self, pf_vector_t pose)
 {  
   double p;
   map_cell_t *cell;  
-
-  cell = map_get_cell(sensor->map, pose.v[0], pose.v[1], pose.v[2]);
+  
+  cell = map_get_cell(self->map, pose.v[0], pose.v[1], pose.v[2]);
   if (!cell)
     return 0;
 
   if (cell->occ_state != -1)
     return 0.01;
   
-  if (sensor->stall == 0)
-  {
-    if (cell->occ_dist < sensor->robot_radius)
-      p = 0.01;
-    else
-      p = 1.0;
-  }
+  if (cell->occ_dist < self->robot_radius)
+    p = 0.01;
   else
-  {
-    if (cell->occ_dist < sensor->robot_radius)
-      p = 1.0;
-    else
-      p = 0.01;
-  }
+    p = 1.0;
 
   //printf("x = %f %f %f p = %f\n", pose.v[0], pose.v[1], pose.v[2], p);
   
