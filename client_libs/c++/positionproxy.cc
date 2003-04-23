649783698dd72aea8056dfe5d944c994888ff030
Brian Gerkey <brian@gerkey.org>
2003-04-23 22:16:12 +0000

    fixed minor PositionProxy::Print() bug

--- client_libs/c++/positionproxy.cc
+++ client_libs/c++/positionproxy.cc
@@ -370,7 +370,7 @@ void PositionProxy::Print()
 {
   printf("#Position(%d:%d) - %c\n", device, index, access);
   puts("#xpos\typos\ttheta\tspeed\tsidespeed\tturn\tstall");
-  printf("%d\t%d\t%u\t%d\t%d\t%d\n", 
+  printf("%d\t%d\t%u\t%d\t%d\t%d\t%u\n", 
          xpos,ypos,theta,speed,sidespeed,turnrate,stall);
 }
 
