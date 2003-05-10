6862bcb661f5fa88b519682bfa6752948ebe206f
Brian Gerkey <brian@gerkey.org>
2003-05-10 00:16:13 +0000

    added LGPL to the client libs

--- client_libs/c++/wifiproxy.cc
+++ client_libs/c++/wifiproxy.cc
@@ -1,7 +1,8 @@
 /*
  *  Player - One Hell of a Robot Server
- *  Copyright (C) 2000  
- *     Brian Gerkey, Kasper Stoy, Richard Vaughan, & Andrew Howard
+ *  Copyright (C) 2000-2003
+ *     John Sweeney, UMASS, Amherst, Laboratory for Perceptual Robotics
+ *     Brian Gerkey & Andrew Howard
  *
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -19,14 +20,31 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+/*
+ *  Player - One Hell of a Robot Server
+ *  Copyright (C) 2003
+ *     John Sweeney, UMASS, Amherst, Laboratory for Perceptual Robotics
+ *     Brian Gerkey & Andrew Howard
+ *                      
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
-/* Copyright (C) 2002
- *   John Sweeney, UMASS, Amherst, Laboratory for Perceptual Robotics
- *
+/* 
  * $Id$
- * 
  * implementation of WiFi proxy class
- *
  */
 
 #include <playerclient.h>
