5c6ec7febfe033ca7bbc2e307a5f5c6f756f168a
Andrew Howard <ahoward@usc.edu>
2003-05-30 06:34:12 +0000

    Fixed bug in model causing over-convergence

--- server/drivers/localization/amcl/models/laser.c
+++ server/drivers/localization/amcl/models/laser.c
@@ -1,3 +1,23 @@
+/*
+ *  Player - One Hell of a Robot Server
+ *  Copyright (C) 2000  Brian Gerkey   &  Kasper Stoy
+ *                      gerkey@usc.edu    kaspers@robotics.usc.edu
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 /**************************************************************************
  * Desc: Sensor model the laser sensor.
  * Author: Andrew Howard
@@ -14,56 +34,56 @@
 #define LASER_MAX_RANGES 401
 
 // Pre-compute the range sensor probabilities
-void laser_precompute(laser_t *sensor);
+void laser_precompute(laser_t *self);
 
 
 // Create an sensor model
 laser_t *laser_alloc(map_t *map, pf_vector_t laser_pose)
 {
-  laser_t *sensor;
+  laser_t *self;
 
-  sensor = calloc(1, sizeof(laser_t));
+  self = calloc(1, sizeof(laser_t));
 
-  sensor->map = map;
-  sensor->laser_pose = laser_pose;
+  self->map = map;
+  self->laser_pose = laser_pose;
 
-  sensor->range_cov = 0.05 * 0.05;
-  sensor->range_bad = 0.20;
+  self->range_cov = 0.10 * 0.10;
+  self->range_bad = 0.50;
 
-  laser_precompute(sensor);
+  laser_precompute(self);
     
-  sensor->range_count = 0;
-  sensor->ranges = calloc(LASER_MAX_RANGES, sizeof(laser_range_t));
+  self->range_count = 0;
+  self->ranges = calloc(LASER_MAX_RANGES, sizeof(laser_range_t));
   
-  return sensor;
+  return self;
 }
 
 
 // Free an sensor model
-void laser_free(laser_t *sensor)
+void laser_free(laser_t *self)
 {
-  free(sensor->lut_probs);
-  free(sensor->ranges);
-  free(sensor);
+  free(self->lut_probs);
+  free(self->ranges);
+  free(self);
   return;
 }
 
 
 // Clear all existing range readings
-void laser_clear_ranges(laser_t *sensor)
+void laser_clear_ranges(laser_t *self)
 {
-  sensor->range_count = 0;
+  self->range_count = 0;
   return;
 }
 
 
 // Set the laser range readings that will be used.
-void laser_add_range(laser_t *sensor, double range, double bearing)
+void laser_add_range(laser_t *self, double range, double bearing)
 {
   laser_range_t *beam;
   
-  assert(sensor->range_count < LASER_MAX_RANGES);
-  beam = sensor->ranges + sensor->range_count++;
+  assert(self->range_count < LASER_MAX_RANGES);
+  beam = self->ranges + self->range_count++;
   beam->range = range;
   beam->bearing = bearing;
 
@@ -75,7 +95,7 @@ void laser_add_range(laser_t *sensor, double range, double bearing)
 // We use a two-dimensional array over (model_range, obs_range).
 // currently, only the difference (obs_range - model_range) is significant,
 // so this is somewhat inefficient.
-void laser_precompute(laser_t *sensor)
+void laser_precompute(laser_t *self)
 {
   double max;
   double c, z, p;
@@ -84,28 +104,31 @@ void laser_precompute(laser_t *sensor)
   
   // Laser max range and resolution
   max = 8.00;
-  sensor->lut_res = 0.01;
+  self->lut_res = 0.01;
   
-  sensor->lut_size = (int) ceil(max / sensor->lut_res);
-  sensor->lut_probs = malloc(sensor->lut_size * sensor->lut_size * sizeof(sensor->lut_probs[0]));
+  self->lut_size = (int) ceil(max / self->lut_res);
+  self->lut_probs = malloc(self->lut_size * self->lut_size * sizeof(self->lut_probs[0]));
 
-  for (i = 0; i < sensor->lut_size; i++)
+  for (i = 0; i < self->lut_size; i++)
   {
-    mrange = i * sensor->lut_res;
+    mrange = i * self->lut_res;
     
-    for (j = 0; j < sensor->lut_size; j++)
+    for (j = 0; j < self->lut_size; j++)
     {
-      orange = j * sensor->lut_res;
+      orange = j * self->lut_res;
 
       // TODO: proper sensor model (using Kolmagorov?)
       // Simple gaussian model
-      c = sensor->range_cov;
+      c = self->range_cov;
       z = orange - mrange;
-      p = sensor->range_bad + (1 - sensor->range_bad) *
-        (1 / (sqrt(2 * M_PI * c)) * exp(-(z * z) / (2 * c)));
+      p = self->range_bad + (1 - self->range_bad) * exp(-(z * z) / (2 * c));
 
-      sensor->lut_probs[i + j * sensor->lut_size] = p;
+      //printf("%f %f %f\n", orange, mrange, p);
+      //assert(p >= 0 && p <= 1.0);
+      
+      self->lut_probs[i + j * self->lut_size] = p;
     }
+    //printf("\n");
   }
 
   // TODO
@@ -116,27 +139,32 @@ void laser_precompute(laser_t *sensor)
 
 
 // Determine the probability for the given range reading
-inline double laser_sensor_prob(laser_t *sensor, double obs_range, double map_range)
+inline double laser_sensor_prob(laser_t *self, double obs_range, double map_range)
 {
   int i, j;
+  double p;
 
-  i = (int) (map_range / sensor->lut_res + 0.5);
-  j = (int) (obs_range / sensor->lut_res + 0.5);
+  i = (int) (map_range / self->lut_res + 0.5);
+  j = (int) (obs_range / self->lut_res + 0.5);
 
   assert(i >= 0);
-  if (i >= sensor->lut_size)
-    i = sensor->lut_size - 1;
+  if (i >= self->lut_size)
+    i = self->lut_size - 1;
 
   assert(j >= 0);
-  if (j >= sensor->lut_size)
-    j = sensor->lut_size - 1;
+  if (j >= self->lut_size)
+    j = self->lut_size - 1;
 
-  return sensor->lut_probs[i + j * sensor->lut_size];
+  p = self->lut_probs[i + j * self->lut_size];
+
+  //assert(p >= 0 && p <= 1.0);
+  
+  return p;
 }
 
 
 // Determine the probability for the given pose
-double laser_sensor_model(laser_t *sensor, pf_vector_t pose)
+double laser_sensor_model(laser_t *self, pf_vector_t pose)
 {
   int i;
   double p;
@@ -144,17 +172,25 @@ double laser_sensor_model(laser_t *sensor, pf_vector_t pose)
   laser_range_t *obs;
 
   // Take account of the laser pose relative to the robot
-  pose = pf_vector_coord_add(sensor->laser_pose, pose);
+  pose = pf_vector_coord_add(self->laser_pose, pose);
 
   p = 1.0;
   
-  for (i = 0; i < sensor->range_count; i++)
+  for (i = 0; i < self->range_count; i++)
   {
-    obs = sensor->ranges + i;
-    map_range = map_calc_range(sensor->map,
+    obs = self->ranges + i;
+
+    map_range = map_calc_range(self->map,
                                pose.v[0], pose.v[1], pose.v[2] + obs->bearing, 8.0);
 
-    p *= laser_sensor_prob(sensor, obs->range, map_range);
+    if (obs->range >= 8.0 && map_range >= 8.0)
+      p *= 1.0;
+    else if (obs->range >= 8.0 && map_range < 8.0)
+      p *= self->range_bad;
+    else if (obs->range < 8.0 && map_range >= 8.0)
+      p *= self->range_bad;
+    else
+      p *= laser_sensor_prob(self, obs->range, map_range);
   }
 
   //printf("%e\n", p);
