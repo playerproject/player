350f92db219c054686a9aa3b5ad09401e5bf4510
Andrew Howard <ahoward@usc.edu>
2003-07-15 16:07:05 +0000

    Added null interface

--- server/drivers/shell/writelog_manager.cc
+++ server/drivers/shell/writelog_manager.cc
@@ -36,6 +36,10 @@
 #include <string.h>
 #include <unistd.h>
 
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "player.h"
 #include "device.h"
 #include "deviceregistry.h"
@@ -109,13 +113,24 @@ WriteLogManager::~WriteLogManager()
 int WriteLogManager::Startup()
 {
   // Open the file
-  this->file = gzopen(this->filename, "w+");
+  this->file = fopen(this->filename, "w+");
   if (this->file == NULL)
   {
     PLAYER_ERROR2("unable to open [%s]: %s\n", this->filename, strerror(errno));
     return -1;
   }
 
+  // Write the file header
+  fprintf(this->file, "## Player version %s \n", VERSION);
+  fprintf(this->file, "## File version %s \n", "0.0.0");
+
+  // Start our own driver thread
+  if (pthread_create(&this->thread, NULL, &DummyMain, this) != 0)
+  {
+    PLAYER_ERROR("unable to create device thread");
+    return -1;
+  }
+  
   return 0;
 }
 
@@ -124,11 +139,33 @@ int WriteLogManager::Startup()
 // Finalize the driver
 int WriteLogManager::Shutdown()
 {
+  // Stop the driver thread
+  pthread_cancel(this->thread);
+  if (pthread_join(this->thread, NULL) != 0)
+    PLAYER_WARN("error joining device thread");
+
   // Close the file
-  gzclose(this->file);
+  fclose(this->file);
   this->file = NULL;
   
   return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+// Dummy main
+void *WriteLogManager::DummyMain(void *_this)
+{
+  ((WriteLogManager*) _this)->Main();
+  return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+// Driver thread
+void WriteLogManager::Main()
+{
+  // TODO
 }
 
 
@@ -152,11 +189,67 @@ void WriteLogManager::Write(void *data, size_t size,
   port = 0;
   
   // Write header info
-  gzprintf(this->file, "%14.3f %s %d %s %02d %14.3f ",
+  fprintf(this->file, "%14.3f %s %d %s %02d %14.3f ",
            (double) stime.tv_sec + (double) stime.tv_usec * 1e-6,
            host, port, iface.name, id->index,
            (double) sec + (double) usec * 1e-6);
+
+  // Write the data
+  switch (id->code)
+  {
+    case PLAYER_POSITION_CODE:
+      this->WritePosition((player_position_data_t*) data, size);
+      break;
+    case PLAYER_LASER_CODE:
+      this->WriteLaser((player_laser_data_t*) data, size);
+      break;
+  }
+
+  fprintf(this->file, "\n");
   
   return;
 }
 
+
+////////////////////////////////////////////////////////////////////////////
+// Signed int conversion macros
+#define N_INT16(x) ((int16_t) ntohs(x))
+#define N_UINT16(x) ((uint16_t) ntohs(x))
+#define N_INT32(x) ((int32_t) ntohl(x))
+#define N_UINT32(x) ((uint32_t) ntohl(x))
+
+
+////////////////////////////////////////////////////////////////////////////
+// Unit conversion macros
+#define MM_M(x) ((x) / 1000.0)
+#define DEG_RAD(x) ((x) * M_PI / 180.0)
+
+
+////////////////////////////////////////////////////////////////////////////
+// Write position data to file
+void WriteLogManager::WritePosition(player_position_data_t *data, size_t size)
+{
+  fprintf(this->file, "%+07.3f %+07.3f %+04.3f %+07.3f %+07.3f %+07.3f %d",
+          MM_M(N_INT32(data->xpos)), MM_M(N_INT32(data->ypos)), DEG_RAD(N_INT32(data->yaw)),
+          MM_M(N_INT32(data->xspeed)), MM_M(N_INT32(data->yspeed)), DEG_RAD(N_INT32(data->yspeed)),
+          data->stall);
+
+  return;
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+// Write laser data to file
+void WriteLogManager::WriteLaser(player_laser_data_t *data, size_t size)
+{
+  int i;
+  
+  fprintf(this->file, "%+07.4f %+07.4f %+07.4f %04d ",
+          DEG_RAD(N_INT16(data->min_angle) * 0.01), DEG_RAD(N_INT16(data->max_angle) * 0.01),
+          DEG_RAD(N_UINT16(data->resolution) * 0.01), N_UINT16(data->range_count));
+
+  for (i = 0; i < ntohs(data->range_count); i++)
+    fprintf(this->file, "%.3f %2d ", MM_M(N_UINT16(data->ranges[i])), data->intensity[i]);
+
+  return;
+}
