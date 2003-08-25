e951d1c0942314bf5dbdd95f87e6f236fe9fcff9
Richard Vaughan <vaughan.sourceforge..ca>
2003-08-25 21:24:01 +0000

    made C++ tests a  little more forgiving

--- client_libs/c++/test/test_fiducialfinder.cc
+++ client_libs/c++/test/test_fiducialfinder.cc
@@ -39,16 +39,17 @@ test_fiducial(PlayerClient* client, int index)
       FAIL();
       return -1;
     }
-  PASS();
+  else
+    PASS();
 
   TEST("getting field of view (FOV)" );
   puts( "" );
   if( fp.PrintFOV() < 0 )
     {
       FAIL();
-      return -1;
+      //return -1;
     }
-  PASS();
+  else PASS();
 
   double original_min_range  = fp.min_range;
   double original_max_range  = fp.max_range;
@@ -65,28 +66,30 @@ test_fiducial(PlayerClient* client, int index)
   if( fp.SetFOV( goal_min_range, goal_max_range, goal_view_angle) < 0 )
     {
       FAIL();
-      return -1;
+      //return -1;
     }
-
-  if( !(fp.min_range == goal_min_range && 
-	fp.max_range == goal_max_range && 
-	fp.view_angle == goal_view_angle ))
+  else
     {
-      puts( "\nwarning: resulting FOV differs from requested values" );
-
-      if( fp.min_range != goal_min_range )
-	printf( "FOV min range %.2f doesn't match requested value %.2f\n",
-	fp.min_range, goal_min_range  );
-    
-      if( fp.max_range != goal_max_range )
-      printf( "FOV max range %.2f doesn't match requested value %.2f\n",
-	       fp.max_range, goal_max_range );
-
-      if( fp.view_angle != goal_view_angle )
-      printf( "FOV min range %.2f doesn't match requested value %.2f\n",
-	     fp.view_angle, goal_view_angle );
+      if( !(fp.min_range == goal_min_range && 
+	    fp.max_range == goal_max_range && 
+	    fp.view_angle == goal_view_angle ))
+	{
+	  puts( "\nwarning: resulting FOV differs from requested values" );
+	  
+	  if( fp.min_range != goal_min_range )
+	    printf( "FOV min range %.2f doesn't match requested value %.2f\n",
+		    fp.min_range, goal_min_range  );
+	  
+	  if( fp.max_range != goal_max_range )
+	    printf( "FOV max range %.2f doesn't match requested value %.2f\n",
+		    fp.max_range, goal_max_range );
+	  
+	  if( fp.view_angle != goal_view_angle )
+	    printf( "FOV min range %.2f doesn't match requested value %.2f\n",
+		    fp.view_angle, goal_view_angle );
+	}
+      PASS();
     }
-    PASS();
 
     // wait for a few cycles so we can see the change
     for(int i=0; i < 10; i++)
@@ -97,42 +100,80 @@ test_fiducial(PlayerClient* client, int index)
 	< 0 )
       {
 	FAIL();
-	return -1;
+	//return -1;
       }
-    
-    if( !(fp.min_range == original_min_range && 
-	  fp.max_range == original_max_range && 
-	  fp.view_angle == original_view_angle ))
+    else
       {
-	puts( "\nwarning: resulting FOV differs from original values" );
-	
-	if( fp.min_range != original_min_range )
-	  printf( "FOV min range %.2f doesn't match original value %.2f\n",
-		  fp.min_range, original_min_range  );
-	
-	if( fp.max_range != original_max_range )
-	  printf( "FOV max range %.2f doesn't match original value %.2f\n",
-		  fp.max_range, original_max_range );
-
-	if( fp.view_angle != original_view_angle )
-	  printf( "FOV min range %.2f doesn't match original value %.2f\n",
-		  fp.view_angle, original_view_angle );
+	if( !(fp.min_range == original_min_range && 
+	      fp.max_range == original_max_range && 
+	      fp.view_angle == original_view_angle ))
+	  {
+	    puts( "\nwarning: resulting FOV differs from original values" );
+	    
+	    if( fp.min_range != original_min_range )
+	      printf( "FOV min range %.2f doesn't match original value %.2f\n",
+		      fp.min_range, original_min_range  );
+	    
+	    if( fp.max_range != original_max_range )
+	      printf( "FOV max range %.2f doesn't match original value %.2f\n",
+		      fp.max_range, original_max_range );
+	    
+	    if( fp.view_angle != original_view_angle )
+	      printf( "FOV min range %.2f doesn't match original value %.2f\n",
+		      fp.view_angle, original_view_angle );
+	  }
+	PASS();
       }
-    PASS();
     
     // wait for a few cycles so we can see the change
     for(int i=0; i < 10; i++)
       client->Read();
     
-    for( int i=0; i<10; i++ )
+    // attempt to send a message
+    TEST("broadcasting a message");
+    
+    player_fiducial_msg_t msg;
+    memset( &msg, 0, sizeof(msg) );
+    msg.target_id = -1; // broadcast
+    snprintf( (char*)&msg.bytes, PLAYER_FIDUCIAL_MAX_MSG_LEN, 
+	      "broadcast message" );
+    msg.len = (uint8_t)strlen((char*)&msg.bytes);
+    msg.consume = 1;
+    
+    if( fp.SendMessage(&msg) < 0 )
       {
-	player_fiducial_msg_t msg;
-	memset( &msg, 0, sizeof(msg) );
-	msg.target_id = 13;
+	FAIL();
+	puts( "Messaging not supported" );
+      }
+    else
+      PASS();
+    
+    // attempt to read a message
+    TEST("reading a message");
+    
+    player_fiducial_msg_t recv;
+    if( fp.RecvMessage(&recv,1) < 0 )
+      {
+	FAIL();
+	puts( "Messaging not supported" );
+      }
+    else
+      {
+	printf( "Message received %d bytes (%s)\n", recv.len, recv.bytes );
+	PASS();
+      }
+    
+    // send a message to each detected fiducial in turn
+    for( int i=0; i<fp.count; i++ )
+      {
+	// wait for a few cycles so we can see the messages happen
+	for(int j=0; j < 10; j++)
+	  client->Read();
+	
+	msg.target_id = fp.beacons[i].id;
 	snprintf( (char*)&msg.bytes, PLAYER_FIDUCIAL_MAX_MSG_LEN, 
-		  "test message %d", i );
+		  "message to %d", msg.target_id );
 	msg.len = (uint8_t)strlen((char*)&msg.bytes);
-	msg.consume = 1;
 	
 	// attempt to send a message
 	TEST("broadcasting a message");
@@ -145,25 +186,8 @@ test_fiducial(PlayerClient* client, int index)
 	  }
 	else
 	  PASS();
-	
-	// attempt to read a message
-	TEST("reading a message");
-	
-	player_fiducial_msg_t recv;
-	if( fp.RecvMessage(&recv,1) < 0 )
-	  {
-	    FAIL();
-	    puts( "Messaging not supported" );
-	    break;
-	  }
-	else
-	  PASS();
-
-	// wait for a few cycles so we can see the messages
-	for(int i=0; i < 5; i++)
-	  client->Read();
       }
-
+    
 
     TEST("unsubscribing");
     if((fp.ChangeAccess(PLAYER_CLOSE_MODE,&access) < 0) ||
