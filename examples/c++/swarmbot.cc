ab65fa42b2a3b2dd7e4bab701ffa4a5136873b75
Richard Vaughan <vaughan.sourceforge..ca>
2003-09-20 22:21:03 +0000

    stage1p4 tweaks and swarmbot demo

--- examples/c++/swarmbot.cc
+++ examples/c++/swarmbot.cc
@@ -110,11 +110,13 @@ int main(int argc, char **argv)
       bots[r].pc = new PlayerClient(host,port++);      
       bots[r].fp = new FiducialProxy(bots[r].pc,0,'a');
       bots[r].pp = new PositionProxy(bots[r].pc,0,'a');
-      bots[r].bpr = new BlinkenlightProxy(bots[r].pc,0,'a');
-      bots[r].bpg = new BlinkenlightProxy(bots[r].pc,1,'a');
-      bots[r].bpb = new BlinkenlightProxy(bots[r].pc,2,'a');
+      //bots[r].bpr = new BlinkenlightProxy(bots[r].pc,0,'a');
+      //bots[r].bpg = new BlinkenlightProxy(bots[r].pc,1,'a');
+      //bots[r].bpb = new BlinkenlightProxy(bots[r].pc,2,'a');
+      bots[r].sp = new SonarProxy(bots[r].pc,0,'a');
 
-      //bots[r].sp = new SonarProxy(bots[r].pc,0,'a');
+      // slow the read rate a bit
+      bots[r].pc->SetFrequency( 3 );
 
       // create some dummy messages in the store
       for( int s=0; s<storelen; s++ )
@@ -126,7 +128,7 @@ int main(int argc, char **argv)
       for( int a=0; a<5; a++ )
 	if( bots[r].pc->Read()) exit(1);
  
-      //bots[r].sp->GetSonarGeom();
+      bots[r].sp->GetSonarGeom();
    }
   
 
@@ -192,100 +194,128 @@ int main(int argc, char **argv)
 	      if( bots[r].fp->beacons[f].id == upstream_bot )
 		direction = bots[r].fp->beacons[f].pose[1];  
 	    } 
-	  
-	  // a little potential field algorithm for dispersal
-	  double dx=0.0, dy=0.0;
-	  
-	  
-	  // for each neighbor detected
-	  for( int s=0; s< bots[r].fp->count; s++ )
-	    {
-	      double range = (bots[r].fp->beacons[s].pose[0] / 1000.0);
-	      double bearing = DTOR(bots[r].fp->beacons[s].pose[1]);   
-	      
-	      range = 1.0 / (range*range);
 
-	      //range = range - 1.5;
-	      
-	      dx -= range * cos( bearing ); 	  
-	      dy -= range * sin( bearing ); 	   
-	    }
+	  //printf( "upstream bot is %d at direction %d\n", 
+	  //  upstream_bot, direction  );
 	 
-	  /*
-	  // for each sonar reading
-	  for( int s=0; s< bots[r].sp->range_count; s++ )
+	  if( direction != 0 )
 	    {
-	      double range = bots[r].sp->ranges[s] / 1000.0;
-	      double bearing = DTOR(bots[r].sp->sonar_pose.poses[s][2]);   
-	      
-	      //range = range - 1.5;
-	      
-	      //if( range < 0.5 ) range = 0.0;
+	      //  printf( "direction %d\n", direction );
 
-	      dx += range * cos( bearing ); 	  
-	      dy += range * sin( bearing ); 	   
+	      // threshold our turnrate
+	      int turnrate = direction;
+	      if( turnrate > 45 ) turnrate = 45.0;
+	      if( turnrate < -45 ) turnrate = -45.0;
+	      
+	      //bots[r].pp->SetSpeed( 0, turnrate );
+
+	      /*
+	      // enable the red light if we're pointing to the source
+	      if( fabs(direction) < 5 )
+		{
+		  bots[r].bpr->SetLight( true, 0 );
+		  //done = 1;
+		  //exit( 0 );
+		}
+	      else
+		bots[r].bpr->SetLight( false, 0 );
+	      */
+	      
+	      // enable the red light if we're happy with our position
+	      //if( forwards == 0 )
+	      //	bots[r].bpr->SetLight( true, 0 );
+	      //else
+	      //bots[r].bpr->SetLight( false, 0 );
+	      
+	      // transmit my range estimate to the broadcast address (-1)
+	      if( lowrange < 10000 )
+		{
+		  gradient_t grad;
+		  grad.type = 1;
+		  grad.range = lowrange;
+	      
+		  player_fiducial_msg_t send;
+		  send.target_id = -1;
+		  memcpy(send.bytes,&grad,sizeof(grad));
+		  send.len = sizeof(grad);
+		  send.intensity = 200;
+		  
+		  bots[r].fp->SendMessage( &send, true );
+		}
 	    }
-	  */
+	    //else
+	    {	 
+	      //bots[r].bpr->SetLight( false, 0 );
 
-	  // threshold our turnrate
-	  int turnrate = direction;
-	  if( turnrate > 100 ) turnrate = 100.0;
-	  if( turnrate < -100 ) turnrate = -100.0;
-	  
-	  // move!
-	  //bots[r].pp->SetSpeed( 200*dx, 200*dy, turnrate );
-	  //bots[r].pp->SetSpeed( 200*dx, 200*dy, 0 );
-	  
-	  double resultant_angle = atan2(dy,dx);
-	  double resultant_distance = hypot(dx,dy );
-
-	  int forwards = 0;
-	  if( fabs(resultant_angle) < 0.1 )
-	    forwards = (int)(20 * resultant_distance);
-	  
-	  if( fabs(forwards) < 25 ) forwards = 0;
-	  if( forwards > 400 ) forwards = 400;
-	  if( forwards < -400 ) forwards = -400;
-
-
-	  //bots[r].pp->SetSpeed( 0,0, 2 * (int)RTOD(resultant_angle)  );
-	  bots[r].pp->SetSpeed( forwards,0, 2.0 * (int)RTOD(resultant_angle)  );
-
-	  //printf( "dx %.2f dy %.2f  = %.2f radians %.2fm  forwards %d\n",
-	  //  dx, dy, atan2(dy,dx), hypot(dx,dy), forwards );
-
-
-
-	  // enable the blue light if we're pointing to the source
-	  if( fabs(direction) < 3 )
-	    bots[r].bpb->SetLight( true, 0 );
-	  else
-	    bots[r].bpb->SetLight( false, 0 );
-
-
-	  // enable the red light if we're happy with our position
-	  if( forwards == 0 )
-	    bots[r].bpr->SetLight( true, 0 );
-	  else
-	    bots[r].bpr->SetLight( false, 0 );
-
-	  // transmit my range estimate to the broadcast address (-1)
-	  if( lowrange < 10000 )
-	    {
-	      gradient_t grad;
-	      grad.type = 1;
-	      grad.range = lowrange;
+	      // a little potential field algorithm for dispersal
+	      double dx=0.0, dy=0.0;
 	      
-	      player_fiducial_msg_t send;
-	      send.target_id = -1;
-	      memcpy(send.bytes,&grad,sizeof(grad));
-	      send.len = sizeof(grad);
-	      send.intensity = 200;
 	      
-	      bots[r].fp->SendMessage( &send, true );
+	      // for each neighbor detected
+	      for( int s=0; s< bots[r].fp->count; s++ )
+		{
+		  double range = (bots[r].fp->beacons[s].pose[0] / 1000.0);
+		  double bearing = DTOR(bots[r].fp->beacons[s].pose[1]);   
+		  
+		  range = 1.0 / (range*range);
+		  
+		  dx -= range * cos( bearing ); 	  
+		  dy -= range * sin( bearing ); 	   
+		}
+	      
+	      
+	      // for each sonar reading
+	      for( int s=0; s< bots[r].sp->range_count; s++ )
+		{
+		  double range = bots[r].sp->ranges[s] / 1000.0;
+		  double bearing = DTOR(bots[r].sp->sonar_pose.poses[s][2]);   
+		  
+		  range = 1.0 / (range*range);
+		  
+		  //  printf( "sonar %d range %d bearing %d \n", 
+		  //     s, bots[r].sp->ranges[s], 
+		  //     bots[r].sp->sonar_pose.poses[s][2] );
+		  
+		  //printf( "sonar %d range %.3f bearing %.2f \n", 
+		  //     s, range, bearing );
+		  
+		  dx -= range * cos( bearing ); 	  
+		  dy -= range * sin( bearing ); 	   
+		}
+	      
+	      double resultant_angle = atan2(dy,dx);
+	      double resultant_distance = hypot(dx,dy );
+	      
+	      if( resultant_distance < 0.0001 )
+		resultant_angle = 0.0;
+	      
+	      //printf( "resultant_distance: %.4f\n", resultant_distance );
+	      
+	      int forwards = 0;
+	      if( fabs(resultant_angle) < 0.5 )
+		forwards = (int)(50 * resultant_distance);
+	      
+	      //printf( "forwards %d\n", forwards );
+	      
+	      if( fabs(forwards) < 5 ) forwards = 0;
+	      if( forwards > 200 ) forwards = 200;
+	      if( forwards < -200 ) forwards = -200;
+	      
+	      int turnrate = (int)RTOD(resultant_angle);
+	      if( turnrate > 60 ) turnrate = 60;
+	      if( turnrate < -60 ) turnrate = -60;
+
+	      //bots[r].pp->SetSpeed( 0,0, 2 * (int)RTOD(resultant_angle)  );
+	      bots[r].pp->SetSpeed( forwards,0, turnrate);
+	      
+	      //printf( "dx %.2f dy %.2f  = %.2f radians %.2fm  forwards %d\n",
+	      //  dx, dy, atan2(dy,dx), hypot(dx,dy), forwards );
+	      
+	      
+
 	    }
 	}
     }
 }
-
-
+  
+  
