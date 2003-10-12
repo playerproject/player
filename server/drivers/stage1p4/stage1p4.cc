5fba15a78a0f000e639cbccbfc4ad896cf77349f
Richard Vaughan <vaughan.sourceforge..ca>
2003-10-12 19:30:59 +0000

    added subscription server

--- server/drivers/stage1p4/stage1p4.cc
+++ server/drivers/stage1p4/stage1p4.cc
@@ -39,7 +39,7 @@
 #include <sys/ioctl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/soundcard.h>
+//#include <sys/soundcard.h>
 #include <stdio.h>
 #include <math.h>
 #include <sys/time.h> 
@@ -66,6 +66,8 @@ stg_name_id_t* Stage1p4::created_models = NULL;
 int Stage1p4::created_models_count = 0;
 ConfigFile* Stage1p4::config = NULL;
 CWorldFile Stage1p4::wf;
+stg_client_t *Stage1p4::subclient = NULL;
+stg_id_t Stage1p4::world_id = 0;
 
 // signal cacher - when Player gets a SIG_USR2 we save the worldfile
 void catch_sigusr2( int signum )
@@ -108,7 +110,8 @@ Stage1p4::Stage1p4(char* interface, ConfigFile* cf, int section,
       PLAYER_MSG2( "Creating client to Stage server on %s:%d", 
 		   stage_host, stage_port );
       
-      Stage1p4::stage_client = stg_client_create( stage_host, stage_port);
+      Stage1p4::stage_client = 
+	stg_client_create( stage_host, stage_port, STG_TOS_REQUESTREPLY );
       assert(Stage1p4::stage_client);
       
       // load a worldfile and create a passel of Stage models
@@ -122,12 +125,29 @@ Stage1p4::Stage1p4(char* interface, ConfigFile* cf, int section,
       // worldfile into a series of model creation and configuration
       // requests. It creates an array of <name,stageid> pairs.
       wf.Upload( Stage1p4::stage_client, 
-		 &Stage1p4::created_models, &Stage1p4::created_models_count );
+		 &Stage1p4::created_models, 
+		 &Stage1p4::created_models_count,
+		 &Stage1p4::world_id );
 
       // catch USR2 signal. getting this signal makes us SAVE the
       // world state.
       if( signal( SIGUSR2, catch_sigusr2 ) == SIG_ERR )
 	PLAYER_ERROR( "stage1p4 failed to install SAVE signal handler." );
+
+      // create another client, this one in subscription mode
+      //stg_client_t *subcli = 
+      Stage1p4::subclient = 
+	stg_client_create( stage_host, stage_port, STG_TOS_SUBSCRIPTION );
+      
+      assert( Stage1p4::subclient );
+      
+      stg_subscription_t sub;
+      sub.id = Stage1p4::world_id;
+      sub.prop = STG_PROP_TIME;
+      stg_property_subscribe( Stage1p4::subclient,&sub ); 
+      
+
+      this->StartThread();
     } 
 }
 
@@ -147,7 +167,7 @@ int Stage1p4::Setup()
   const char* name = config->ReadString( section, "model", "<no name>" );
   PLAYER_MSG1( "stage1p4 starting device name \"%s\"", name );
   
-  // lookup name (todo - this would be much faster using a hash table)
+  // lookup name (todo - speed this up with a hash table)
   this->stage_id = -1; // invalid
   for( int i=0; i < created_models_count; i++ )
     if( strcmp( created_models[i].name, name ) == 0 )
@@ -173,7 +193,30 @@ int Stage1p4::Setup()
 
 int Stage1p4::Shutdown()
 {
+  this->StopThread();
+
   return 0; //ok
 }
+
+
+void Stage1p4::Main( void )
+{
+  while(1)
+    {
+      // test if we are supposed to cancel
+      pthread_testcancel();
+      //puts( "main" );
+      //sleep(2);
+
+      printf( "reading subscribed property on fd %d\n", Stage1p4::subclient->pollfd.fd );
+      stg_property_t* prop = stg_property_read( Stage1p4::subclient );
+
+      printf( "received property %d:%d\n",
+	      prop->id, prop->property );
+
+      stg_property_free(prop);
+    }
+}
+
 // END CLASS
 
