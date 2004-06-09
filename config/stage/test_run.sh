4ca16aec06c65363099c739e54950939876e3cd2
Richard Vaughan <vaughan@sfu.ca>
2004-06-09 02:44:58 +0000

    more stage testing stuff

---
+++ config/stage/test_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./test_client -i 0 &
+./test_client -i 1 &
+./test_client -i 2 &
+./test_client -i 3 &
+./test_client -i 4 &
+./test_client -i 5 &
+./test_client -i 6 &
+./test_client -i 7 &
+./test_client -i 8 &
+./test_client -i 9 &
+#./test_client -i 10 &
+#./test_client -i 11 &
+#./test_client -i 12 &
+#./test_client -i 13 &
+#./test_client -i 14 &
+#./test_client -i 15 &
+#./test_client -i 16 &
+#./test_client -i 17 &
+#./test_client -i 18 &
+#./test_client -i 19 &
