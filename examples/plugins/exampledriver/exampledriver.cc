e10f64101a8f72f7323761d7aeb521ab78137eed
Andrew Howard <ahoward@usc.edu>
2004-11-11 07:56:10 +0000

    *** empty log message ***

--- examples/plugins/exampledriver/exampledriver.cc
+++ examples/plugins/exampledriver/exampledriver.cc
@@ -85,7 +85,7 @@ void ExampleDriver_Register(DriverTable* table)
 // Constructor.  Retrieve options from the configuration file and do any
 // pre-Setup() setup.
 ExampleDriver::ExampleDriver(ConfigFile* cf, int section)
-    : Driver(cf, section, PLAYER_POSITION_CODE, PLAYER_READ_MODE,
+    : Driver(cf, section, PLAYER_POSITION_CODE, PLAYER_ALL_MODE,
              sizeof(player_position_data_t),sizeof(player_position_cmd_t),10,10)
 {
   // Read an option from the configuration file
