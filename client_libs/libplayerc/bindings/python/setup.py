87a55b54acbf2a2e52e55e7e6a5a878af32bd1e2
Brian Gerkey <brian@gerkey.org>
2005-01-18 17:37:07 +0000

    (hopefully) fixed the broken build when libjpeg isn't installed

--- client_libs/libplayerc/bindings/python/setup.py
+++ client_libs/libplayerc/bindings/python/setup.py
@@ -2,6 +2,11 @@
 from distutils.core import setup, Extension
 
 
+#TODO: handle linking to libjpeg conditionally, depending on whether or not
+#      it was found during configuration.  The easiest way to do this may
+#      be to use libplayerpacket's pkgconfig info.  Until then, I'm
+#      disabling the use of camera decompression in the libplayerc python
+#      bindings.
 module = Extension('_playerc',
                    sources = ['playerc.i'],
                    include_dirs = ['../..', '../../../../server'],
