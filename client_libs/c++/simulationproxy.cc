39132cfa93f36d449382200eba4ba8bce242c536
Richard Vaughan <vaughan@sfu.ca>
2005-02-08 04:59:59 +0000

    fixed teeny bug

--- client_libs/c++/simulationproxy.cc
+++ client_libs/c++/simulationproxy.cc
@@ -72,7 +72,7 @@ int SimulationProxy::SetPose2D( char* identifier, double x, double y, double a )
   req.subtype = PLAYER_SIMULATION_SET_POSE2D;
   strncpy( req.name, identifier, PLAYER_SIMULATION_IDENTIFIER_MAXLEN );
   req.x = htonl( (int32_t)(1000.0 * x) );
-  req.y = htonl( (int32_t)(1000.0 * x) );
+  req.y = htonl( (int32_t)(1000.0 * y) );
   req.a = htonl( (int32_t)RTOD(a) );
 
   return client->Request( m_device_id,
