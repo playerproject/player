448cb14a33df7323eaa7c03902120a43c51fa2dd
Brian Gerkey <brian@gerkey.org>
2005-05-03 00:11:39 +0000

    applied Radu's patch to make map drivers fill in subtype when responding to MAP_GET_INFO request

--- server/drivers/map/mapcspace.cc
+++ server/drivers/map/mapcspace.cc
@@ -482,6 +482,9 @@ MapCspace::HandleGetMapInfo(void *client, void *request, int len)
     return;
   }
 
+  // copy in subtype
+  info.subtype = ((player_map_info_t*)request)->subtype;
+
   // convert to pixels / kilometer
   info.scale = htonl((uint32_t)rint(1e3 / this->resolution));
 
