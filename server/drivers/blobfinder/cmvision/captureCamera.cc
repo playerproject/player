9cc27623c913c5d529980f9fd126db158053c7d3
Toby Collett <tcollett@plan9.net.nz>
2005-05-16 08:54:29 +0000

    moved to new message API - untested, likely to not work

--- server/drivers/blobfinder/cmvision/captureCamera.cc
+++ server/drivers/blobfinder/cmvision/captureCamera.cc
@@ -38,15 +38,21 @@
 #include "conversions.h"
 #include "captureCamera.h"
 #include "playerpacket.h"
+#include "clientdata.h"
 
 extern int global_playerport;
 
-captureCamera::captureCamera(int camera_index) : capture()
+captureCamera::captureCamera(int camera_index) : capture() , Driver(NULL,0)
 {
   this->camera_index = camera_index;
   this->camera = NULL;
   this->camera_time = 0;
   //     cout << "captureCamera::captureCamera() index "<<this->camera_index<<" port "<<  this->camera_port<<endl;
+
+  BaseClient = new ClientDataInternal(this);
+  assert(BaseClient);
+  clientmanager->AddClient(BaseClient);
+
 
   // Subscribe to the camera.
   camera_id.code = PLAYER_CAMERA_CODE;
@@ -59,7 +65,8 @@ captureCamera::captureCamera(int camera_index) : capture()
     camera_open = false;
     return;
   }
-  if (this->camera->Subscribe(this->camera_id) != 0)
+  
+  if (BaseClient->Subscribe(this->camera_id) != 0)
   {
     PLAYER_ERROR("unable to subscribe to camera device");
     camera_open = false;
@@ -71,7 +78,7 @@ captureCamera::captureCamera(int camera_index) : capture()
   // find the camera driver
   sprintf(dname,"%s",deviceTable->GetDriverName(camera_id));
   // gz_camera will lock up if you do this. Others Need it.
-  if (strcmp(dname,"gz_camera"))
+  /*if (strcmp(dname,"gz_camera"))
        this->camera->Wait();
 
   size_t size;
@@ -86,29 +93,38 @@ captureCamera::captureCamera(int camera_index) : capture()
   this->height = ntohs(data.height); 
   this->depth = data.bpp;
    
-  this->image_size = ntohl(data.image_size); 
+  this->image_size = ntohl(data.image_size); */
   /*  cout << "captureCamera::initialize()"<<endl;
       cout << "width " << width<<endl;
       cout << "height " <<height  <<endl;
       cout << "depth " <<depth  <<endl;
       cout << "image_size " <<image_size <<endl;*/
-  YUV=(unsigned char *)malloc(width*height*2);
+  YUV=NULL;
+  FrameData=NULL;
+  /*YUV=(unsigned char *)malloc(width*height*2);
   if (!YUV)
   {
     PLAYER_ERROR("Out of memory");
     camera_open = false;
     return;
-  }
+  }*/
  
   camera_open = true;
 }
 
 captureCamera::~captureCamera()
 {
-  this->camera->Unsubscribe(this->camera_id);
+  // Unsubscribe from devices.
+  BaseClient->Unsubscribe(this->camera_id);
+
+  clientmanager->RemoveClient(BaseClient);
+  delete BaseClient;
+  BaseClient = NULL;
+  
   if (YUV) free(YUV);
   YUV=NULL;
   current=NULL;
+  free(FrameData);
 } 
 
 bool captureCamera::initialize(int nwidth,int nheight)
@@ -118,9 +134,104 @@ bool captureCamera::initialize(int nwidth,int nheight)
      return (camera_open);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Process an incoming message
+int captureCamera::ProcessMessage(ClientData * client, player_msghdr * hdr, uint8_t * data, uint8_t * resp_data, int * resp_len)
+{
+  assert(hdr);
+  assert(data);
+  assert(resp_data);
+  assert(resp_len);
+  assert(*resp_len==PLAYER_MAX_MESSAGE_SIZE);
+  
+  *resp_len = 0;
+
+  /* TODO
+  if (MatchMessage(hdr, PLAYER_MSGTYPE_REQ, PLAYER_BLOBFINDER_GET_GEOM, device_id))
+  {
+  }
+  */
+ 
+  if (MatchMessage(hdr, PLAYER_MSGTYPE_DATA, 0, camera_id))
+  {
+  	assert(hdr->size == sizeof(player_camera_data_t));
+  	player_camera_data_t * cam_data = reinterpret_cast<player_camera_data_t * > (data);
+  	Lock();
+//     struct timeval timestamp;
+//     size_t size;
+//     double t;
+     int w,h;
+#if HAVE_JPEGLIB_H
+     int dst_size;
+     unsigned char * dst;
+#endif
+ 
+     // check if image has changed size
+     w = ntohs(cam_data->width);
+     h = ntohs(cam_data->height);
+     if ((w!=width)||(h!=height)||YUV==NULL)
+	  {
+	       printf("captureCamera::captureFrame() camera resized %dx%d\n",w,h);
+	       // this will lose memory but it hopefully will not happen
+	       // just here 'cause it can happen
+	       // can't free it as CMVisionBF::Main() might be using it
+	       // free(YUV);
+	       if ((w>width)||(h>height))
+	       {
+	    	    if (YUV) free(YUV);
+		    YUV=(unsigned char *)malloc(w*h*2);
+		    if (!YUV)
+		    {
+			PLAYER_ERROR("Out of memory");
+			FrameData= NULL;
+			return 0;
+		    }
+	       }
+	       width = w;
+	       height = h;
+	  }
+     switch (cam_data->compression)
+     {
+     case PLAYER_CAMERA_COMPRESS_RAW:
+        FrameData = convertImageRgb2Yuv422(cam_data->image, width * height);
+        break;
+     case PLAYER_CAMERA_COMPRESS_JPEG:
+#if HAVE_JPEGLIB_H
+	// Create a temp buffer
+	dst_size = width * height * (cam_data->bpp / 8);
+	dst = (unsigned char *)malloc(dst_size);
+
+	// Decompress into temp buffer
+	jpeg_decompress(dst, dst_size, cam_data->image, ntohl(cam_data->image_size));
+
+	FrameData = convertImageRgb2Yuv422(dst, width * height);
+	free(dst);
+#else
+	PLAYER_ERROR("JPEG decompression support was not included at compile-time");
+	FrameData = NULL;
+#endif
+	break;
+     default:
+	PLAYER_ERROR("Unknown compression type");
+	FrameData = NULL;        
+     }
+
+  	Unlock();
+  	return 0;
+  }
+ 
+  return -1;
+}
+
+
 unsigned char *captureCamera::captureFrame()
 {
-     struct timeval timestamp;
+	ProcessMessages();
+	if (NewCamData)
+		return FrameData;
+	else
+		return NULL;
+/*     struct timeval timestamp;
      size_t size;
      double t;
      int w,h;
@@ -191,7 +302,7 @@ unsigned char *captureCamera::captureFrame()
 	PLAYER_ERROR("Unknown compression type");
 	ptr = NULL;        
      }
-     return ptr;
+     return ptr;*/
 }
 
 
