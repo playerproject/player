b2d91eac3aed650e78da74d518ad27343e82566f
Brian Gerkey <brian@gerkey.org>
2005-05-25 18:59:08 +0000

    Removed now-unnecessary tests and replacements for alphasort and scandir.

--- replace/replace.h
+++ replace/replace.h
@@ -76,31 +76,11 @@ int poll (struct pollfd *fds, unsigned long int nfds, int timeout);
 #include <sys/poll.h>  /* for poll(2) */
 #endif // !HAVE_POLL
 
-#if !HAVE_SCANDIR
-#include <sys/types.h>
-#include <dirent.h>
-int scandir(const char *dir, struct dirent ***namelist,
-            int (*select)(const struct dirent *),
-            int (*compar)(const void *, const void *));
-#endif //!HAVE_SCANDIR
-
 #if !HAVE_DIRNAME
   char * dirname (char *path);
 #else
   #include <libgen.h> // for dirname(3)
 #endif // !HAVE_DIRNAME
-
-#if !HAVE_ALPHASORT
-  int alphasort (const void *a, const void *b);
-#else
-  #include <sys/types.h>
-  #include <dirent.h>
-  #ifdef __CYGWIN__
-    #include <mingw/dir.h>
-  #else
-    #include <sys/dir.h>
-  #endif
-#endif
 
 #if !HAVE_CFMAKERAW
   #include <termios.h>
