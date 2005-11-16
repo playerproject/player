8e5620f103c91009efd60b11d5d19419808aa7ea
Brad Kratochvil <brad@kratochvil.name>
2005-11-16 12:59:31 +0000

    documentation update

--- examples/libplayerc/simple.c
+++ examples/libplayerc/simple.c
@@ -1,77 +1,40 @@
-/* 
- *  libplayerc : a Player client library
- *  Copyright (C) Andrew Howard 2002
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-/***************************************************************************
- * Desc: Example program for libplayerc.
- * Author: Andrew Howard
- * Date: 28 May 2002
- * CVS: $Id$
- **************************************************************************/
-
 #include <stdio.h>
-#include "playerc.h"
-#include "playercommon.h"
+#include "libplayerc/playerc.h"
 
-
-int main(int argc, const char **argv)
+int
+main(int argc, const char **argv)
 {
   int i;
-  const char *host;
-  int port;
   playerc_client_t *client;
-  playerc_position_t *position;
+  playerc_position2d_t *position2d;
 
-  host = "localhost";
-  port = 6665;
-    
   // Create a client and connect it to the server.
-  client = playerc_client_create(NULL, host, port);
-  if (playerc_client_connect(client) != 0)
+  client = playerc_client_create(NULL, "localhost", 6665);
+  if (0 != playerc_client_connect(client))
     return -1;
 
-  /*
-  if(playerc_client_datamode(client, PLAYER_DATAMODE_PULL_NEW) < 0)
-    return -1;
-    */
-
-  // Create and subscribe to a position device.
-  position = playerc_position_create(client, 0);
-  if (playerc_position_subscribe(position, PLAYER_ALL_MODE))
+  // Create and subscribe to a position2d device.
+  position2d = playerc_position2d_create(client, 0);
+  if (playerc_position2d_subscribe(position2d, PLAYER_OPEN_MODE))
     return -1;
 
   // Make the robot spin!
-  if (playerc_position_set_cmd_vel(position, 0, 0, DTOR(40.0), 1) != 0)
+  if (0 != playerc_position2d_set_cmd_vel(position2d, 0, 0, DTOR(40.0), 1))
     return -1;
-  
+
   for (i = 0; i < 200; i++)
   {
     // Wait for new data from server
     playerc_client_read(client);
-    
+
     // Print current robot pose
-    printf("position : %f %f %f\n",
-           position->px, position->py, position->pa);
-  } 
+    printf("position2d : %f %f %f\n",
+           position2d->px, position2d->py, position2d->pa);
+  }
 
   // Shutdown
-  playerc_position_unsubscribe(position);
-  playerc_position_destroy(position);
+  playerc_position2d_unsubscribe(position2d);
+  playerc_position2d_destroy(position2d);
   playerc_client_disconnect(client);
   playerc_client_destroy(client);
 
