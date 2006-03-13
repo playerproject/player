977736b2e96bb674814159bd5b7fe0f8221a1fc2
Brian Gerkey <brian@gerkey.org>
2006-03-13 18:39:15 +0000

    added Geoff's docs to libplayertcp

--- libplayertcp/playertcp.h
+++ libplayertcp/playertcp.h
@@ -2,7 +2,7 @@
  *  Player - One Hell of a Robot Server
  *  Copyright (C) 2005 -
  *     Brian Gerkey
- *                      
+ *
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -30,6 +30,42 @@
  * @brief Player TCP library
 
 This library moves messages between Player message queues and TCP sockets.
+
+@section datamode_protocol Data modes
+
+Clients can use two modes for receiving data. These are PUSH and PULL modes.
+There are some important differences between them. These are summarised here.
+These modes only affect the clients' message queues, that is they do not
+affect how messages are received from clients.
+
+In PUSH mode all messages are sent as soon as possible. This is usually when
+there is room in the operating system's buffer for the message. Clients should
+read these messages as usual. For example, the libplayerc client library will
+read exactly one message for each call to the client read function.
+
+In PULL mode:
+<ul>
+<li>Messages are only sent to the client when they are marked as ready in the
+client's message queue.
+<li>PLAYER_MSGTYPE_DATA messages are not marked as ready until the client
+requests data.
+<li>All other messages (PLAYER_MSGTYPE_RESP_ACK and PLAYER_MSGTYPE_RESP_NACK
+messages) are marked as ready upon entering the queue.
+<li>When a PLAYER_PLAYER_REQ_DATA message is received, all messages in the
+client's queue are marked as ready. A PLAYER_MSGTYPE_SYNCH message is pushed
+onto the end of the queue.
+</ul>
+
+A client in PULL mode should request data before performing a read by sending a
+PLAYER_PLAYER_REQ_DATA request message. The client should then continue to
+receive and handle all messages until it receives the PLAYER_MSGTYPE_SYNCH
+message. Note that the PLAYER_MSGTYPE_RESP_ACK for the PLAYER_PLAYER_REQ_DATA
+will come at the <i>end</i> of all other waiting data (but <i>before</i> the
+PLAYER_MSGTYPE_SYNCH message), due to the way the message queue system
+functions. This means that client libraries should read and store all other
+messages when waiting for a PLAYER_MSGTYPE_RESP_ACK, then process them at
+the beginning of their read function after sending the PLAYER_PLAYER_REQ_DATA
+message.
 
 @todo
  - More verbose documentation on this library, including the protocol
@@ -90,7 +126,7 @@ class PlayerTCP
     pthread_t thread;
 
     int Listen(int* ports, int num_ports);
-    MessageQueue* AddClient(struct sockaddr_in* cliaddr, 
+    MessageQueue* AddClient(struct sockaddr_in* cliaddr,
                             unsigned int local_host,
                             unsigned int local_port,
                             int newsock,
