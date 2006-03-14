128b23b55fa3a1ad86276ce1328c13d691ec123a
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2006-03-14 07:21:12 +0000

    Fix state value in SetSpeed() and Goto() functions

--- client_libs/libplayerc++/position2dproxy.cc
+++ client_libs/libplayerc++/position2dproxy.cc
@@ -80,14 +80,14 @@ void
 Position2dProxy::SetSpeed(double aXSpeed, double aYSpeed, double aYawSpeed)
 {
   scoped_lock_t lock(mPc->mMutex);
-  playerc_position2d_set_cmd_vel(mDevice,aXSpeed,aYSpeed,aYawSpeed,0);
+  playerc_position2d_set_cmd_vel(mDevice,aXSpeed,aYSpeed,aYawSpeed,1);
 }
 
 void
 Position2dProxy::GoTo(double aX, double aY, double aYaw)
 {
   scoped_lock_t lock(mPc->mMutex);
-  playerc_position2d_set_cmd_pose(mDevice,aX,aY,aYaw,0);
+  playerc_position2d_set_cmd_pose(mDevice,aX,aY,aYaw,1);
 }
 
 void
