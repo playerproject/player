6fad1d64f6a02ef9c225dc665fda7b6a331962be
Toby Collett <tcollett+player@plan9.net.nz>
2006-05-04 05:58:28 +0000

    fixed some bugs with the client lib relating to timeouts added mising graphics messages to xdr functiontable (they used to be there)

--- libplayerxdr/functiontable.c
+++ libplayerxdr/functiontable.c
@@ -156,6 +156,8 @@ static playerxdr_function_t init_ftable[] =
    (player_pack_fn_t)player_gps_data_pack},
 
   /* graphics2d messages */
+  {PLAYER_GRAPHICS2D_CODE, PLAYER_MSGTYPE_CMD, PLAYER_GRAPHICS2D_CMD_CLEAR,
+   (player_pack_fn_t)player_graphics2d_cmd_points_pack},
   {PLAYER_GRAPHICS2D_CODE, PLAYER_MSGTYPE_CMD, PLAYER_GRAPHICS2D_CMD_POINTS,
    (player_pack_fn_t)player_graphics2d_cmd_points_pack},
   {PLAYER_GRAPHICS2D_CODE, PLAYER_MSGTYPE_CMD, PLAYER_GRAPHICS2D_CMD_POLYGON,
@@ -164,6 +166,8 @@ static playerxdr_function_t init_ftable[] =
    (player_pack_fn_t)player_graphics2d_cmd_polyline_pack},
 
   /* graphics3d messages */
+  {PLAYER_GRAPHICS3D_CODE, PLAYER_MSGTYPE_CMD, PLAYER_GRAPHICS3D_CMD_CLEAR,
+   (player_pack_fn_t)player_graphics3d_cmd_draw_pack},
   {PLAYER_GRAPHICS3D_CODE, PLAYER_MSGTYPE_CMD, PLAYER_GRAPHICS3D_CMD_DRAW,
    (player_pack_fn_t)player_graphics3d_cmd_draw_pack},
 
