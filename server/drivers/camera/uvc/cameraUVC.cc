b2441bee40c8289b36790ed291adb1f30965aad0
Toby Collett <tcollett+player@plan9.net.nz>
2006-10-25 09:57:12 +0000

    Added a new version of the cameraUVC driver that should be more stable, particularly when connecting and disconnecting. Changes made by Luke Gumbley.

--- server/drivers/camera/uvc/cameraUVC.cc
+++ server/drivers/camera/uvc/cameraUVC.cc
@@ -28,206 +28,167 @@
  *                                                                          *
  *  Portions based on the Player/Stage Sample Plugin Driver                 *
  *  Portions based on luvcview by Laurent Pinchart and Michel Xhaard        *
+ *                                                                          *
+ * Updated in September 2006 by Luke Gumbley to enhance stability
+ *                                                                          *
+ *                                                                          *
 \****************************************************************************/
 
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <math.h>
+/** @ingroup drivers */
+/** @{ */
+/** @defgroup driver_camerauvc camerauvc
+ * @brief USB video (UVC) camera interface
+ 
+%Device driver for webcams supporting the USB video standard. Works with
+recent Logitech cams (QuickCam Fusion, QuickCam Orbit, QuickCam Pro for 
+Notebooks and QuickCam Pro 5000) and hopefully many USB webcams released 
+2007 onward will support the standard.
+ 
+@par Compile-time dependencies
 
-#include <libplayercore/playercore.h>
-#include <libplayercore/error.h>
+- none
 
-// To prevent the C++ compiler's name mangling from causing 
-// problems with the C names here
-extern "C"
+@par Requires
+
+- none
+
+@par Provides
+
+- @ref interface_camera
+
+@par Configuration requests
+
+- none
+
+@par Configuration file options
+
+- port (string)
+  - Default: "/dev/video0"
+  - Linux video device entry
+- size (integer tuple)
+  - Default: [320 240]
+  - size of the image to grab, most cameras should at least support [640 480] as well
+
+@par Example
+
+@verbatim
+driver
+(
+  name "camerauvc"
+  provides ["camera:0"]
+  port "/dev/video0"
+  size [640 480]
+)
+@endverbatim
+
+@authors Raymond Sheh, Modified by Luke Gumbley
+
+ */
+/** @} */
+
+#include "cameraUVC.h"
+
+Driver* CameraUvc_Init(ConfigFile* cf, int section)
 {
-#include "v4l2uvc.h"
-}
-
-class CameraUVC : public Driver
-{
-  public:
-    // Player must-have methods
-    CameraUVC(ConfigFile* cf, int section);
-    int Setup();
-    int Shutdown();
-
-    /// @brief Message handler
-    public: int ProcessMessage (MessageQueue *resp_queue, player_msghdr *hdr, void *data);
-
-  private: 
-    virtual void Main();
-    int GrabFrame();            // Grab a frame to videoIn
-    void WriteData();            // Write new data to the client (through the server)
-
-    player_camera_data_t data;  // Data to send to client (through the server)
-    struct vdIn *videoIn;        // Video capture structure
-    char videoDevice[512];      // Video capture device (eg. /dev/video0)
-    int format;                  // Video capture format (only V4L2_PIX_FMT_MJPEG tested)
-    int grabmethod;              // Video capture grab method (only "1" tested)
-    int width;                  // Video frame width (no error checking for invalid values!)
-    int height;                  // Video frame height (no error checking for invalid values!) 
-};
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Player plugin driver standard functions
-////////////////////////////////////////////////////////////////////////////////
-Driver* CameraUVC_Init(ConfigFile* cf, int section)
-{
-  return((Driver*)(new CameraUVC(cf, section)));
+	return((Driver*)(new CameraUvc(cf, section)));
 }
 
 void CameraUVC_Register(DriverTable* table)
 {
-  table->AddDriver("camerauvc", CameraUVC_Init);
+	table->AddDriver("camerauvc", CameraUvc_Init);
 }
 
-#if 0
-extern "C" {
-  int player_driver_init(DriverTable* table)
-  {
-    CameraUVC_Register(table);
-    return(0);
-  } 
+extern "C"
+{
+	int player_driver_init(DriverTable* table)
+	{
+		CameraUVC_Register(table);
+		return(0);
+	} 
 }
-#endif
 
-////////////////////////////////////////////////////////////////////////////////
-// Constructor.  Also reads config file. 
-////////////////////////////////////////////////////////////////////////////////
-CameraUVC::CameraUVC(ConfigFile* cf, int section)
+CameraUvc::CameraUvc(ConfigFile* cf, int section)
     : Driver(cf, section, true, PLAYER_MSGQUEUE_DEFAULT_MAXLEN, PLAYER_CAMERA_CODE)
 {
-  format = V4L2_PIX_FMT_MJPEG;
-  grabmethod = 1;
-  strncpy(this->videoDevice, cf->ReadString(section, "port", "/dev/video0"), sizeof(this->videoDevice));
-  this->width = cf->ReadTupleInt(section, "size", 0, 320);
-  this->height = cf->ReadTupleInt(section, "size", 1, 240);
-  return;
+	ui=new UvcInterface(cf->ReadString(section, "port", "/dev/video0"),
+						cf->ReadTupleInt( section, "size", 0, 320),
+						cf->ReadTupleInt( section, "size", 1, 240));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set up the device.  Return 0 if things go well, and -1 otherwise.
 // Also starts the mainloop. 
 ////////////////////////////////////////////////////////////////////////////////
-int CameraUVC::Setup()
+int CameraUvc::Setup()
 {   
-  printf("CameraUVC: Driver initialising\n");
+	printf("CameraUvc: Driver initialising\n");
 
-  videoIn = new vdIn;
-  if (init_videoIn(videoIn, (char *) videoDevice, width, height, format, grabmethod) < 0)
-  {
-    PLAYER_ERROR("CameraUVC: Error setting up video capture!");
-    this->SetError(-1);
-    return(-1); 
-  }
+	if(ui->Open()==-1)
+	{
+		PLAYER_ERROR("CameraUvc: Error setting up video capture!");
+		SetError(-1);
+		return -1; 
+	}
 
-  this->StartThread();
-  printf("CameraUVC: Driver initialisation done\n");
-  return(0);
+	StartThread();
+	printf("CameraUvc: Driver initialisation done\n");
+	
+	return 0;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Shutdown the device. 
 // Also stops the mainloop. 
 ////////////////////////////////////////////////////////////////////////////////
-int CameraUVC::Shutdown()
+int CameraUvc::Shutdown()
 {
-  printf("CameraUVC: Driver shutting down\n");
+	printf("CameraUvc: Driver shutting down\n");
 
-  StopThread();
-  close_v4l2(videoIn);
-  delete videoIn;
-  videoIn = NULL;
+	StopThread();
+	ui->Close();
 
-  printf("CameraUVC: Driver shutdown complete\n");
-  return(0);
+	printf("CameraUvc: Driver shutdown complete\n");
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main function (mainloop)
 ////////////////////////////////////////////////////////////////////////////////
-void CameraUVC::Main() 
+void CameraUvc::Main() 
 {
-  while (true)
-  {
-    // Test if we are supposed to cancel this thread.
-    pthread_testcancel();
+	while (true)
+	{
+		// Test if we are supposed to cancel this thread.
+		pthread_testcancel();
 
-    // Process any pending requests.
-    ProcessMessages();
+		// Process any pending requests.
+		ProcessMessages();
 
-    // Grab the next frame (blocking)
-    this->GrabFrame();
+		ui->Read();
 
-    // Write data to server
-    this->WriteData();
-  }
+		// Store image details
+		data.width=ui->GetWidth(); 
+		data.height=ui->GetHeight(); 
+		data.bpp=24; 
+		data.format=PLAYER_CAMERA_FORMAT_RGB888;
+		data.fdiv=1; 
+		data.compression=PLAYER_CAMERA_COMPRESS_JPEG;
+		data.image_count=ui->GetFrameSize(); 
+		ui->CopyFrame(data.image);
+  
+		// Work out the data size; do this BEFORE byteswapping
+		size_t size=sizeof(this->data) - sizeof(this->data.image) + this->data.image_count;
+
+		// Write data to the client (through the server)
+		Publish (device_addr,NULL,PLAYER_MSGTYPE_DATA,PLAYER_CAMERA_DATA_STATE,&data,size,NULL);
+	}
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Process requests.  Returns 1 if the configuration has changed.
 // Ignore all requests for now. 
 ////////////////////////////////////////////////////////////////////////////////
-int CameraUVC::ProcessMessage (MessageQueue *resp_queue, player_msghdr *hdr, void *data)
+int CameraUvc::ProcessMessage (MessageQueue *resp_queue, player_msghdr *hdr, void *data)
 {
   return -1;
 }
-
-////////////////////////////////////////////////////////////////////////////////
-// Store an image frame into videoIn
-////////////////////////////////////////////////////////////////////////////////
-int CameraUVC::GrabFrame()
-{
-  if (videoIn->signalquit)
-  {
-    if (uvcGrab(videoIn) < 0) 
-    {
-      PLAYER_ERROR("CameraUVC: Error grabbing frame!");
-      return (-1); 
-    }
-    // Image is now in videoIn->framebuffer in raw form and 
-    // videoIn->tmpbuffer in JPEG form. 
-  }
-  else
-  {
-    // signalquit? 
-    PLAYER_ERROR("CameraUVC: Error, got signalquit?");
-    return (-1); 
-  }
-  return 0;
-}
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Write the new frame back to the client (through the server)
-////////////////////////////////////////////////////////////////////////////////
-void CameraUVC::WriteData()
-{
-  size_t size;
-  // Store image details
-  this->data.width = this->width; 
-  this->data.height = this->height; 
-  this->data.bpp = 24; 
-  this->data.format = PLAYER_CAMERA_FORMAT_RGB888;
-  this->data.fdiv = 1; 
-  this->data.compression = PLAYER_CAMERA_COMPRESS_JPEG;
-  this->data.image_count = videoIn->buf.bytesused + DHT_SIZE; 
-
-  // Store image
-  memcpy(this->data.image, videoIn->tmpbuffer, this->data.image_count);
-  
-  // Work out the data size; do this BEFORE byteswapping
-  size = sizeof(this->data) - sizeof(this->data.image) + this->data.image_count;
-
-  // Write data to the client (through the server)
-  Publish (device_addr, NULL, PLAYER_MSGTYPE_DATA, PLAYER_CAMERA_DATA_STATE, &data, size,NULL);
-  return;
-}
-
