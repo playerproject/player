ea3044eb8672928f353b9c3d7f1175a2a57dbd0d
Radu Bogdan Rusu <radu.b.rusu@gmail.com>
2007-02-07 18:02:33 +0000

    added support for SCIP2 thanks to the excellent work done by our student, Nico Blodow :) also cleaned the code a bit and fixed some bugs.

--- server/drivers/laser/urglaserdriver.cc
+++ server/drivers/laser/urglaserdriver.cc
@@ -26,7 +26,8 @@
  * @brief Hokuyo URG laser range-finder
 
 The urglaser driver controls the Hokuyo URG scanning laser range-finder.
-Communication with the laser can be either via USB or RS232.
+Communication with the laser can be either via USB or RS232. The driver
+supports both Hokuyo's SCIP1 and 2 protocols.
 
 @par Compile-time dependencies
 
@@ -60,7 +61,7 @@ Communication with the laser can be either via USB or RS232.
     the robot to which the laser is attached).
 
 - min_angle, max_angle (angle float)
-  - Default: [-2.0 2.0] (or [-115.0 115.0] in degrees)
+  - Default: [-2.094 2.094] (or [-120.0 120.0] in degrees)
   - Minimum and maximum scan angles to return
 
 - use_serial (integer)
@@ -84,12 +85,10 @@ driver
 )
 @endverbatim
 
-@author Toby Collett
+@author Toby Collett, Nico Blodow
 
 */
 /** @} */
-
-
 
 #include <unistd.h>
 #include <string.h>
@@ -108,10 +107,14 @@ using namespace std;
 
 #include "urg_laser.h"
 
+#define URG04_MIN_STEP 44
+#define URG04_MAX_STEP 725
+
 #include <libplayercore/playercore.h>
 
-class URGLaserDriver : public Driver {
-public:
+class URGLaserDriver : public Driver
+{
+  public:
 
 	// Constructor;
 	URGLaserDriver(ConfigFile* cf, int section);
@@ -127,7 +130,7 @@ public:
                                player_msghdr * hdr,
                                void * data);
 
-private:
+  private:
 	// Main function for device thread.
 	virtual void Main();
 
@@ -139,7 +142,8 @@ private:
 	player_laser_config_t Conf;
 
 	bool UseSerial;
-	int BaudRate;
+	int BaudRate, min_i, max_i;
+	float user_min_angle, user_max_angle;
 	char * Port;
 };
 
@@ -147,173 +151,203 @@ private:
 ////////////////////////////////////////////////////////////////////////////////
 // Constructor.  Retrieve options from the configuration file and do any
 // pre-Setup() setup.
-URGLaserDriver::URGLaserDriver(ConfigFile* cf, int section)
+URGLaserDriver::URGLaserDriver (ConfigFile* cf, int section)
 : Driver(cf, section, false, PLAYER_MSGQUEUE_DEFAULT_MAXLEN, PLAYER_LASER_CODE)
 {
+  //init vars
+  memset (&Data, 0, sizeof (Data));
+  memset (&Geom, 0, sizeof (Geom));
+  memset (&Conf, 0, sizeof (Conf));
+  Geom.size.sw = (0.050);
+  Geom.size.sl = (0.050);
 
-    //init vars
-	memset(&Data, 0, sizeof(Data));
-	memset(&Geom, 0, sizeof(Geom));
-	memset(&Conf, 0, sizeof(Conf));
-	Geom.size.sw = (0.050);
-	Geom.size.sl = (0.050);
+  Readings = new urg_laser_readings_t;
+  assert (Readings);
 
-	Readings = new urg_laser_readings_t;
-	assert(Readings);
+  // read options from config file
+  Geom.pose.px = (cf->ReadTupleLength (section, "pose", 0, 0));
+  Geom.pose.py = (cf->ReadTupleLength (section, "pose", 1, 0));
+  Geom.pose.pa = (cf->ReadTupleAngle  (section, "pose", 2, 0));
 
-    // read options from config file
-	Geom.pose.px = (cf->ReadTupleLength(section,"pose",0,0));
-	Geom.pose.py = (cf->ReadTupleLength(section,"pose",1,0));
-	Geom.pose.pa = (cf->ReadTupleAngle(section,"pose",2,0));
+  // set up config structure
+  Conf.min_angle = cf->ReadAngle (section, "min_angle", DTOR (-120));
+  Conf.max_angle = cf->ReadAngle (section, "max_angle", DTOR (120));
+  user_min_angle = Conf.min_angle;
+  user_max_angle = Conf.max_angle;
+  
+//  Conf.resolution = DTOR (270.0/769.0);
+  Conf.resolution = DTOR (360.0/1024.0);
+  Conf.max_range = 4.0;
+  Conf.range_res = 0.001;
+  Conf.intensity = 0;
+	
+  int b = cf->ReadInt (section, "baud", 115200);
+  switch (b)
+  {
+    case 115200:
+      BaudRate = B115200;
+      break;
+    case 57600:
+      BaudRate = B57600;
+      break;
+    case 19200:
+      BaudRate = B19200;
+      break;
+    default:
+      PLAYER_WARN1 ("ignoring invalid baud rate %d", b);
+      BaudRate = B115200;
+      break;
+  }
 
-	//set up config structure
-	Conf.min_angle = cf->ReadAngle(section,"min_angle",DTOR(-115));
-	Conf.max_angle = cf->ReadAngle(section,"max_angle",DTOR(115));
-	Conf.resolution = DTOR(270.0/769.0);
-        Conf.max_range = 4.0;
-	Conf.range_res = 0.001;
-	Conf.intensity = 0;
-
-	int b = cf->ReadInt(section, "baud", 115200);
-	switch(b)
-	{
-		case 115200:
-			BaudRate = B115200;
-			break;
-		case 57600:
-			BaudRate = B57600;
-			break;
-		case 19200:
-			BaudRate = B19200;
-			break;
-		default:
-			PLAYER_WARN1("ignoring invalid baud rate %d", b);
-			BaudRate = B115200;
-			break;
-	}
-
-	Port = strdup(cf->ReadString(section, "port", "/dev/ttyACM0"));
-	UseSerial = (cf->ReadInt(section, "use_serial", 0)==1);
-
-    return;
+  Port = strdup (cf->ReadString (section, "port", "/dev/ttyACM0"));
+  UseSerial = (cf->ReadInt (section, "use_serial", 0) == 1);
 }
 
-URGLaserDriver::~URGLaserDriver()
+////////////////////////////////////////////////////////////////////////////////
+URGLaserDriver::~URGLaserDriver ()
 {
-	delete Readings;
+  delete Readings;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set up the device.  Return 0 if things go well, and -1 otherwise.
-int URGLaserDriver::Setup() {
-	//config data
-	if(Laser.Open(Port,UseSerial,BaudRate) < 0)
-	{
-		this->SetError(1);
-		return -1;
-	}
+int
+  URGLaserDriver::Setup ()
+{
+  //config data
+  if (Laser.Open (Port, UseSerial, BaudRate) < 0)
+  {
+    this->SetError(1);
+    return -1;
+  }
+  Laser.GetSensorConfig (&Conf);
+  
+  // Solve the min/max angle problem
+  min_i = static_cast<int> (round (384 + Conf.min_angle/Conf.resolution));
+  max_i = static_cast<int> (round (384 + Conf.max_angle/Conf.resolution));
+  
+  // For ancient firmware versions, set some hard limits on the min/max angle capabilities
+  if (min_i < URG04_MIN_STEP)
+    min_i = URG04_MIN_STEP;
+  if (max_i > URG04_MAX_STEP)
+    max_i = URG04_MAX_STEP;
 
+  int user_min_i = static_cast<int> (round (384 + user_min_angle/Conf.resolution));
+  int user_max_i = static_cast<int> (round (384 + user_max_angle/Conf.resolution));
+  
+  if (user_min_i > user_max_i)
+    user_min_i = user_max_i;
+    
+  // We restrict the URG 04-LX to its capabilities
+  if (user_min_i < min_i)
+  {
+    PLAYER_WARN2 ("restricting the lower index bound to %d from %d", min_i, user_min_i);
+    user_min_i = min_i;
+  }
+  min_i = user_min_i;
 
-    // Start the device thread; spawns a new thread and executes
-    // ExampleDriver::Main(), which contains the main loop for the driver.
-	StartThread();
-
-
-    return(0);
+  if (user_max_i > max_i)
+  {
+    PLAYER_WARN2 ("restricting the upper index bound to %d from %d", max_i, user_max_i);
+    user_max_i = max_i;
+  }
+  max_i = user_max_i;
+  
+  Conf.min_angle = (min_i - 384) * Conf.resolution;
+  Conf.max_angle = (max_i - 384) * Conf.resolution;
+  
+  // Start the device thread; spawns a new thread and executes
+  // ExampleDriver::Main(), which contains the main loop for the driver.
+  StartThread ();
+  
+  return (0);
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Shutdown the device
-int URGLaserDriver::Shutdown() {
+int
+  URGLaserDriver::Shutdown ()
+{
   // Stop and join the driver thread
-  StopThread();
+  StopThread ();
 
-  Laser.Close();
+  Laser.Close ();
 
-  return(0);
+  return (0);
 }
 
 
-int URGLaserDriver::ProcessMessage(MessageQueue* resp_queue,
+////////////////////////////////////////////////////////////////////////////////
+int
+  URGLaserDriver::ProcessMessage (MessageQueue* resp_queue,
                                   player_msghdr * hdr,
                                   void * data)
 {
-	if(Message::MatchMessage(hdr, PLAYER_MSGTYPE_REQ,
-                           PLAYER_LASER_REQ_GET_GEOM,
-                           this->device_addr))
-	{
-		Publish(device_addr,resp_queue, PLAYER_MSGTYPE_RESP_ACK,hdr->subtype,&Geom,sizeof(Geom),NULL);
-	}
-	else if(Message::MatchMessage(hdr, PLAYER_MSGTYPE_REQ,
-		PLAYER_LASER_REQ_GET_CONFIG, this->device_addr))
-	{
-		Publish(device_addr,resp_queue, PLAYER_MSGTYPE_RESP_ACK,hdr->subtype,&Conf,sizeof(Conf),NULL);
-	}
-	else if(Message::MatchMessage(hdr, PLAYER_MSGTYPE_REQ,
-		PLAYER_LASER_REQ_GET_ID, this->device_addr))
-	{
-	    player_laser_get_id_config_t player_ID_conf;
-	    // Get laser identification information
-	    player_ID_conf.serial_number = Laser.GetIDInfo ();
+  if (Message::MatchMessage (hdr, PLAYER_MSGTYPE_REQ,
+                             PLAYER_LASER_REQ_GET_GEOM, this->device_addr))
+    Publish (device_addr,resp_queue, PLAYER_MSGTYPE_RESP_ACK,
+             hdr->subtype, &Geom, sizeof (Geom), NULL);
+    
+  else if (Message::MatchMessage (hdr, PLAYER_MSGTYPE_REQ,
+		                  PLAYER_LASER_REQ_GET_CONFIG, this->device_addr))
+  {
+    Publish (device_addr,resp_queue, PLAYER_MSGTYPE_RESP_ACK,
+             hdr->subtype, &Conf, sizeof (Conf), NULL);
+  }
+  else if (Message::MatchMessage (hdr, PLAYER_MSGTYPE_REQ,
+		                  PLAYER_LASER_REQ_GET_ID, this->device_addr))
+  {
+    player_laser_get_id_config_t player_ID_conf;
+    // Get laser identification information
+    player_ID_conf.serial_number = Laser.GetIDInfo ();
 		
-            Publish (device_addr, resp_queue, PLAYER_MSGTYPE_RESP_ACK, hdr->subtype, 
-		     &player_ID_conf, sizeof (player_ID_conf), NULL);
-	}
-	else
-	{
-		return -1;
-	}
-	return 0;
+    Publish (device_addr, resp_queue, PLAYER_MSGTYPE_RESP_ACK, hdr->subtype, 
+             &player_ID_conf, sizeof (player_ID_conf), NULL);
+  }
+  else
+    return (-1);
+  
+  return (0);
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main function for device thread
-void URGLaserDriver::Main()
+void
+  URGLaserDriver::Main ()
 {
-	// The main loop; interact with the device here
-	for(;;)	{
-    	// test if we are supposed to cancel
-    	pthread_testcancel();
+  // The main loop; interact with the device here
+  for (;;)
+  {
+    // test if we are supposed to cancel
+    pthread_testcancel ();
 
-		// Process any pending messages
-		ProcessMessages();
+    // Process any pending messages
+    ProcessMessages ();
 
-		int min_i = static_cast<int> (384 + Conf.min_angle/Conf.resolution);
-		int max_i = static_cast<int> (384 + Conf.max_angle/Conf.resolution);
+    // update device data
+    Laser.GetReadings (Readings, min_i, max_i);
+    Data.min_angle = Conf.min_angle;
+    Data.max_angle = Conf.max_angle;
+    
+    // TODO: check this value
+    Data.max_range    = Conf.max_range;
+    Data.resolution   = Conf.resolution;
+    Data.ranges_count = (max_i - min_i) + 1;
+    
+    for (unsigned int i = 0; i < Data.ranges_count; ++i)
+    {
+        
+      Data.ranges[i]  = Readings->Readings[i+min_i] < 20 ? (Data.max_range*1000) : (Readings->Readings[i+min_i]);
+      Data.ranges[i] /= 1000;
+    }
+    Publish (device_addr, NULL, PLAYER_MSGTYPE_DATA, PLAYER_LASER_DATA_SCAN,
+             &Data, sizeof(player_laser_data_t), NULL);
 
-		if (min_i > max_i)
-			min_i = max_i;
-		if (min_i < 0)
-			min_i = 0;
-
-		if (max_i > 769)
-			max_i = 769;
-
-
-		// update device data
-		Laser.GetReadings(Readings);
-		Data.min_angle = Conf.min_angle;
-		Data.max_angle = Conf.max_angle;
-                // TODO: check this value
-                Data.max_range = 4.0;
-		Data.resolution = Conf.resolution;
-		Data.ranges_count = max_i - min_i;
-
-		for (unsigned int i = 0; i < Data.ranges_count; ++i)
-		{
-			Data.ranges[i] = Readings->Readings[i+min_i] < 20 ? (4095) : (Readings->Readings[i+min_i]);
-			Data.ranges[i]/=1000;
-		}
-		Publish(device_addr, NULL, PLAYER_MSGTYPE_DATA, PLAYER_LASER_DATA_SCAN,
-			&Data, sizeof(player_laser_data_t), NULL);
-		//printf("Put Data to client\n");
-
-    	// Sleep (you might, for example, block on a read() instead)
-    	//usleep(10);
-
-	}
+    // Sleep (you might, for example, block on a read() instead)
+    //usleep(10);
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -321,19 +355,20 @@ void URGLaserDriver::Main()
 //  new instances of the driver
 ////////////////////////////////////////////////////////////////////////////////
 
-//Factory creation function. This functions is given as an argument when
+// Factory creation function. This functions is given as an argument when
 // the driver is added to the driver table
-Driver* URGLaserDriver_Init(ConfigFile* cf, int section) {
-    // Create and return a new instance of this driver
-	return((Driver*)(new URGLaserDriver(cf, section)));
+Driver*
+  URGLaserDriver_Init(ConfigFile* cf, int section)
+{
+  // Create and return a new instance of this driver
+  return ((Driver*)(new URGLaserDriver (cf, section)));
 }
 
-//Registers the driver in the driver table. Called from the
+// Registers the driver in the driver table. Called from the
 // player_driver_init function that the loader looks for
-int URGLaserDriver_Register(DriverTable* table) {
-	table->AddDriver("urglaser", URGLaserDriver_Init);
-  return 0;
+int 
+  URGLaserDriver_Register (DriverTable* table)
+{
+  table->AddDriver ("urglaser", URGLaserDriver_Init);
+  return (0);
 }
-
-
-
