9fc58f1d8fafb930ec5b63425451cb552cbde1fc
Radu Bogdan Rusu <radu.b.rusu@gmail.com>
2007-02-07 18:02:33 +0000

    added support for SCIP2 thanks to the excellent work done by our student, Nico Blodow :) also cleaned the code a bit and fixed some bugs.

--- server/drivers/laser/urg_laser.h
+++ server/drivers/laser/urg_laser.h
@@ -1,32 +1,35 @@
 #include <stdio.h>
+#include <libplayercore/playercore.h>
 
 #define MAX_READINGS 769
 
 typedef struct urg_laser_readings
 {
-    unsigned short Readings[MAX_READINGS];
+  unsigned short Readings[MAX_READINGS];
 } urg_laser_readings_t;
 
 class urg_laser
 {
-	public:
-		urg_laser();
-		~urg_laser();
+  public:
+  	urg_laser();
+	~urg_laser();
 		
-		int Open(const char * PortName, int use_serial, int baud);
-                int Close();
-                int ChangeBaud(int curr_baud, int new_baud, int timeout);
-		int ReadUntil(int fd, unsigned char *buf, 
-			      int len, int timeout);
+	int Open(const char * PortName, int use_serial, int baud);
+        int Close();
+        int ChangeBaud(int curr_baud, int new_baud, int timeout);
+	int ReadUntil(int fd, unsigned char *buf, int len, int timeout);
 		
+	int ReadUntil_nthOccurence(int file, int n, char c);
 		
-		bool PortOpen();
+	bool PortOpen();
 
-		int GetReadings (urg_laser_readings_t * readings);
-		int GetIDInfo   ();
-		
-				
-	private:
-		FILE * laser_port;
-		
+	int GetReadings     (urg_laser_readings_t * readings, int min_i, int max_i);
+	int GetIDInfo       ();
+	float GetMaxRange   ();
+	int GetSensorConfig (player_laser_config_t *cfg);
+	int GetSCIPVersion  ();
+
+  private:
+  	int SCIP_Version;
+	FILE * laser_port;
 };
