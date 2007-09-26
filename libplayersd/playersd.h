38339ac41a536000c5891f99311006f39ed9804e
Brian Gerkey <brian@gerkey.org>
2007-09-26 00:17:33 +0000

    added docs for playersd

--- libplayersd/playersd.h
+++ libplayersd/playersd.h
@@ -40,6 +40,21 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/*
+ * Interface to libplayersd
+ *
+ * $Id$
+ */
+
+/** @defgroup libplayersd libplayersd
+ * @brief Player service discovery library
+
+This library provides service discovery capabilities for Player devices.
+*/
+
+/** @ingroup libplayersd
+@{ */
+
 #ifndef _PLAYER_SD_H
 #define _PLAYER_SD_H
 
@@ -53,75 +68,77 @@ extern "C" {
 #define PLAYER_SD_NAME_MAXLEN 256
 #define PLAYER_SD_TXT_MAXLEN 256
 
-// A device, represented by its name and address
+/// A device, represented by its name and address
 typedef struct
 {
   char name[PLAYER_SD_NAME_MAXLEN];
   player_devaddr_t addr;
 } player_sd_device_t;
 
-// Service discovery object
+/// Service discovery object
 typedef struct
 {
-  // Opaque pointer to underlying zeroconf client object.  Contents
-  // will vary by zeroconf implementation.
+  /// Opaque pointer to underlying zeroconf client object.  Contents
+  /// will vary by zeroconf implementation.
   void* sdRef;
-  // List of devices discovered by browsing
+  /// List of devices discovered by browsing
   player_sd_device_t* devs;
-  // Number of devices discovered
+  /// Number of devices discovered
   size_t numdevs;
 } player_sd_t;
 
-// Initialize service discovery, passing back a pointer that will be passed
-// into all future calls.  Returns NULL on failure.
+/// Initialize service discovery, passing back a pointer that will be passed
+/// into all future calls.  Returns NULL on failure.
 player_sd_t* player_sd_init(void);
 
-// Finalize service discovery, freeing associated resources.
+/// Finalize service discovery, freeing associated resources.
 void player_sd_fini(player_sd_t* sd);
 
-// Register the named device.  Returns 0 on success, non-zero on error.
-// Name may be automatically changed in case of conflict.
+/// Register the named device.  Returns 0 on success, non-zero on error.
+/// Name may be automatically changed in case of conflict.
 int player_sd_register(player_sd_t* sd, 
                        const char* name, 
                        player_devaddr_t addr);
 
-// Unregister (terminate) the named device.  Returns 0 on success, non-zero 
-// on error.  Name may be automatically changed in case of conflict.
+/// Unregister (terminate) the named device.  Returns 0 on success, non-zero 
+/// on error.  Name may be automatically changed in case of conflict.
 int player_sd_unregister(player_sd_t* sd, 
                          const char* name, 
                          player_devaddr_t addr);
 
-// Prototype for a callback function that can be invoked when devices are
-// added or removed.
+/// Prototype for a callback function that can be invoked when devices are
+/// added or removed.
 typedef void (*player_sd_browse_callback_fn_t)(player_sd_t* sd,
                                                const char* name,
                                                player_devaddr_t addr);
 
-// Browse for player devices.  Browses for timeout ms, accruing the results
-// into the sd object.  If keepalive is non-zero, then the browsing session
-// is left open and can be updated by future calls to player_sd_update().
-// Otherwise, the browsing session is closed before returning.
-// If cb is non-NULL, then it is registered and
-// invoked whenever new device notifications are received (call
-// player_sd_update to give this a chance to happen).  Returns 0 on
-// success, non-zero on error.
+/// Browse for player devices.  Browses for timeout ms, accruing the results
+/// into the sd object.  If keepalive is non-zero, then the browsing session
+/// is left open and can be updated by future calls to player_sd_update().
+/// Otherwise, the browsing session is closed before returning.
+/// If cb is non-NULL, then it is registered and
+/// invoked whenever new device notifications are received (call
+/// player_sd_update to give this a chance to happen).  Returns 0 on
+/// success, non-zero on error.
 int player_sd_browse(player_sd_t* sd,
                      int timeout, 
                      int keepalive,
                      player_sd_browse_callback_fn_t cb);
 
-// Check for new device updates, waiting for timeout ms.  Contents of sd
-// are updated, and if a callback was passed to player_sd_browse, then this
-// function is also called for each discovered device.  Only makes sense to
-// call this function after a call to player_sd_browse.  Returns 0 on
-// success, non-zero on error.
+/// Check for new device updates, waiting for timeout ms.  Contents of sd
+/// are updated, and if a callback was passed to player_sd_browse, then this
+/// function is also called for each discovered device.  Only makes sense to
+/// call this function after a call to player_sd_browse.  Returns 0 on
+/// success, non-zero on error.
 int player_sd_update(player_sd_t* sd, int timeout);
 
-// Stop browsing.  Returns 0 on success, non-zero on error.
+/// Stop browsing.  Returns 0 on success, non-zero on error.
 int player_sd_browse_stop(player_sd_t* sd);
 
 #ifdef __cplusplus
 }
 #endif
+
+/** @} */
 
 #endif
