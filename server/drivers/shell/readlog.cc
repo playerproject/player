3ef3aa7cf1994e3ca86c440fefa94e339853b4ef
Toby Collett <tcollett+player@plan9.net.nz>
2008-04-13 04:40:06 +0000

    applied [ 1908780 ] LogProxy fix

--- server/drivers/shell/readlog.cc
+++ server/drivers/shell/readlog.cc
@@ -696,6 +696,11 @@ void ReadLog::Main()
         // File is done, so just loop forever, unless we're on auto-rewind,
         // or until a client requests rewind.
         reading_configs = false;
+
+        // deactivate driver so clients subscribing to the log interface will notice
+        if(!this->autorewind && !this->rewind_requested)
+          this->enable=false;
+
         while(!this->autorewind && !this->rewind_requested)
         {
           usleep(100000);
