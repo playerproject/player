71867f805eba888837522bc709eae2611fd134fd
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2008-05-03 05:50:09 +0000

    Fixed bad escape sequence for Windows install dir.

--- cmake/internal/SetupDirectories.cmake
+++ cmake/internal/SetupDirectories.cmake
@@ -1,6 +1,6 @@
 # Default installation directory, based on operating system
 IF (PLAYER_OS_WIN)
-    SET (CMAKE_INSTALL_PREFIX "C:\Program Files\Player" CACHE PATH "Installation directory")
+    SET (CMAKE_INSTALL_PREFIX "C:\\Program Files\\Player" CACHE PATH "Installation directory")
 ELSE (PLAYER_OS_WIN)
     SET (CMAKE_INSTALL_PREFIX "/usr/local" CACHE PATH "Installation directory")
 ENDIF (PLAYER_OS_WIN)
