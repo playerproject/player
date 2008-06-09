941d056a5a01e984e0a1cb8ea0fcb784321d94e4
Toby Collett <tcollett@plan9.net.nz>
2008-06-09 07:25:44 +0000

    merged nav fixes 6424,6435,6446 and 6448 from 2-1 patches

--- server/drivers/position/nav200/nav200.cc
+++ server/drivers/position/nav200/nav200.cc
@@ -1,6 +1,7 @@
 #include "nav200.h"
 
 
+
 Nav200::Nav200()
 {
 }
@@ -585,6 +586,27 @@ int Nav200::WriteCommand(char mode, char function, int dataLength, uint8_t * dat
   player_opaque_data_t mData;
   mData.data_count = length;
   mData.data = buffer;
+
+  // before we send a command to the NAV200, flush and data coming in from the remote file
+  sn200->InQueue->SetFilter(opaque_id.host, opaque_id.robot, opaque_id.interf, opaque_id.index, PLAYER_MSGTYPE_DATA, PLAYER_OPAQUE_DATA_STATE);
+  
+  //puts("waiting for data");
+  sn200->ProcessMessages();
+  
+  // before we send a command to the NAV200, flush and data coming in from the remote file
+  if (bytesReceived)
+  {
+
+    do
+    {
+        PLAYER_WARN1("Buffer was not empty on NAV200 (%d), flushing",bytesReceived);
+        bytesReceived = 0;
+    	usleep(100000);
+    	sn200->ProcessMessages();
+    } while (bytesReceived != 0);
+  }
+  sn200->InQueue->ClearFilter();
+  
   opaque->PutMsg(sn200->InQueue, PLAYER_MSGTYPE_CMD, PLAYER_OPAQUE_CMD_DATA, reinterpret_cast<void*>(&mData), 0, NULL);
   
 /*  if((length && (write(fd, buffer, length)) < length))
@@ -626,12 +648,6 @@ int Nav200::ReadFromNav200(int timeout_usec)
   
   for (;;)
   {
-    gettimeofday(&now,NULL);
-    if ((now.tv_sec - start.tv_sec) * 1e6 + now.tv_usec - start.tv_usec > timeout_usec)
-    {
-      fprintf(stderr,"Timed out waiting for packet %d uSecs passed\n",static_cast<int>((now.tv_sec - start.tv_sec) * 1e6 + now.tv_usec - start.tv_usec));
-      return -1;
-    }
     //puts("waiting for data");
     sn200->ProcessMessages();
     
@@ -721,6 +737,13 @@ int Nav200::ReadFromNav200(int timeout_usec)
 
         return 1;
       }
+    }
+    gettimeofday(&now,NULL);
+    if ((now.tv_sec - start.tv_sec) * 1e6 + now.tv_usec - start.tv_usec > timeout_usec)
+    {
+      fprintf(stderr,"Timed out waiting for packet %d uSecs passed\n",static_cast<int>((now.tv_sec - start.tv_sec) * 1e6 + now.tv_usec - start.tv_usec));
+      sn200->InQueue->ClearFilter();
+      return -1;
     }
     usleep(1000);
   }
