b2d736e2f4a40a90f025f0739cb6a4b45a3cb78d
Geoffrey Biggs <geoffrey.biggs@aist.go.jp>
2008-07-03 02:26:35 +0000

    Added separate options for building RTK and including it in drivers. Now playerv builds by default again.

--- cmake/internal/GeneralCompileOptions.cmake
+++ cmake/internal/GeneralCompileOptions.cmake
@@ -3,7 +3,12 @@
 SET (DEBUG_LEVEL NONE CACHE STRING "Level of debug code to be compiled: none, low, medium or high")
 
 IF (WITH_GTK)
-    OPTION (INCLUDE_RTKGUI "Include the RTK GUI in the server." OFF)
+    OPTION (INCLUDE_RTK "Build the RTK GUI library." ON)
+    IF (INCLUDE_RTK)
+        OPTION (INCLUDE_RTKGUI "Include the RTK GUI in the server." OFF)
+    ELSE (INCLUDE_RTK)
+        SET (INCLUDE_RTKGUI OFF CACHE BOOL "Include the RTK GUI in the server." FORCE)
+    ENDIF (INCLUDE_RTK)
 ENDIF (WITH_GTK)
 
 OPTION (BUILD_SHARED_LIBS "Build the Player libraries as shared libraries." ON)
