ba052683fdca6d6f21aa2809fe5f56671b3c2ec4
Toby Collett <tcollett+player@plan9.net.nz>
2009-07-24 13:54:27 +0000

    made headers for erratic consistant

--- server/drivers/mixed/erratic/motorpacket.cc
+++ server/drivers/mixed/erratic/motorpacket.cc
@@ -1,23 +1,26 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:1; -*-
 
-/**
-	*  Videre Erratic robot driver for Player
-	*
-	*  This program is free software; you can redistribute it and/or modify
-	*  it under the terms of the GNU General Public License as published by
-	*  the Free Software Foundation; either version 2 of the License, or
-	*  (at your option) any later version.
-	*
-	*  This program is distributed in the hope that it will be useful,
-	*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	*  GNU General Public License for more details.
-	*
-	*  You should have received a copy of the GNU General Public License
-	*  along with this program; if not, write to the Free Software
-	*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-	*
-**/
+/*
+ *  Player - One Hell of a Robot Server
+ *  Copyright (C) 2000
+ *     Brian Gerkey, Kasper Stoy, Richard Vaughan, & Andrew Howard
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
 #include <stdio.h>
 #include <limits.h>
