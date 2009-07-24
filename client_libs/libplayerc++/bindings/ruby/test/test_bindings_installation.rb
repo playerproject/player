ffd43e5031e06ad5d2476f3551c4aac549e32d36
Toby Collett <tcollett+player@plan9.net.nz>
2009-07-24 11:28:20 +0000

    moving examples to examples tree fixing up license headers for client lib bindings

--- client_libs/libplayerc++/bindings/ruby/test/test_bindings_installation.rb
+++ client_libs/libplayerc++/bindings/ruby/test/test_bindings_installation.rb
@@ -1,7 +1,8 @@
+#!/usr/bin/ruby
 #/*
 # *  Player - One Hell of a Robot Server
-# *  Copyright (C) 2004
-# *     Andrew Howard
+# *  Copyright (C) 2009
+# *     Jordi Polo
 # *                      
 # *
 # *  This library is free software; you can redistribute it and/or
@@ -18,35 +19,25 @@
 # *  License along with this library; if not, write to the Free Software
 # *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # */
+def error
+  puts "Player bindings not installed"
+end
 
-# Desc: Test the laser
-# Author: Andrew Howard
-# Date: 15 Sep 2004
-# CVS: $Id$
+begin
+  
+  require 'playercpp'
 
-from playerc import *
+  if Playercpp::constants.size > 0
+    puts "Player bindings are installed"
+    classes = Playercpp.constants.select {|c| Class === Playerc.const_get(c)}
+#    puts classes
+  else
+    error
+  end
+rescue LoadError
+  error
+rescue
+  error
+end
 
 
-def test_laser(client, index, context):
-    """Basic test of the laser interface."""
-
-    laser = playerc_laser(client, index)
-    if laser.subscribe(PLAYERC_OPEN_MODE) != 0:
-        raise playerc_error_str()    
-
-    for i in range(10):
-
-        while 1:
-            id = client.read()
-            if id == laser.info.id:
-                break
-
-        if context:
-            print context,            
-        print "laser: [%14.3f] [%d]" % (laser.info.datatime, laser.scan_count),
-        for i in range(3):
-            print "[%06.3f, %06.3f]" % (laser.scan[i][0], laser.scan[i][1]),
-        print
-
-    laser.unsubscribe()    
-    return
