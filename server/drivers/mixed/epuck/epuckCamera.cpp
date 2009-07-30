db835ad7cebb31b5e446a92f1e84e6377b167192
Toby Collett <tcollett@plan9.net.nz>
2009-07-30 10:31:19 +0000

    relicensed epuck driver as GPL v2 with permission from the author. Licensed the epuck example under BSD license

--- server/drivers/mixed/epuck/epuckCamera.cpp
+++ server/drivers/mixed/epuck/epuckCamera.cpp
@@ -1,7 +1,7 @@
 /* Copyright 2008 Renato Florentino Garcia <fgar.renato@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3, as
+ * it under the terms of the GNU General Public License version 2, as
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
