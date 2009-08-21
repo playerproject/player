4555e698c639439f49c4e291117feea57d3718fc
Jordi Polo <robotos@users.sourceforge.net>
2009-08-21 03:48:00 +0000

    make libplayerc++ ruby example work

--- examples/ruby/libplayerc++_ruby_example.rb
+++ examples/ruby/libplayerc++_ruby_example.rb
@@ -28,10 +28,10 @@
 require 'playercpp'
 
 # Create a client object and connect it
-robot = Playercpp::PlayerClient('localhost')
+robot = Playercpp::PlayerClient.new('localhost')
 
 # Create a proxy for position2d:0
-pp = Playercpp::Position2dProxy(robot, 0)
+pp = Playercpp::Position2dProxy.new(robot, 0)
 
 # Retrieve the geometry
 pp.RequestGeom()
@@ -39,11 +39,11 @@ size = pp.GetSize()
 puts 'Robot size: (%.3f,%.3f,%.3f)' % [size.sw, size.sl, size.sh]
 
 # Create a proxy for laser:0
-lp = Playercpp::LaserProxy(robot, 0)
+lp = Playercpp::LaserProxy.new(robot, 0)
 
 # Retrieve the geometry
-pose = lp.GetSize()
-puts 'Laser pose: (%.3f,%.3f,%.3f)' % [pose.sw, pose.sl, pose.sh]
+laserSize = lp.GetSize()
+puts 'Laser size: (%.3f,%.3f,%.3f)' % [laserSize.sw, laserSize.sl, laserSize.sh]
 
 # Start the robot turning CCW at 20 deg / sec
 pp.SetSpeed(0.0, 20.0 * Math::PI / 180.0)
