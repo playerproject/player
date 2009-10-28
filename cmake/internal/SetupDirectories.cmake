8b6e2b07e08f83e2eddc4aa15ea611b0bd9b2181
Toby Collett <tcollett@plan9.net.nz>
2009-10-28 22:01:56 +0000

    applied patch 2887703: Player 64 bit install path

--- cmake/internal/SetupDirectories.cmake
+++ cmake/internal/SetupDirectories.cmake
@@ -10,3 +10,12 @@ MESSAGE (STATUS "Player will be installed to ${CMAKE_INSTALL_PREFIX}")
 # Installation prefix for include files
 STRING (TOLOWER ${PROJECT_NAME} projectNameLower)
 SET (PLAYER_INCLUDE_INSTALL_DIR "include/${projectNameLower}-${PLAYER_MAJOR_VERSION}.${PLAYER_MINOR_VERSION}")
+
+IF (PLAYER_PROC_64BIT)
+    SET (PLAYER_LIBRARY_INSTALL_DIR "lib64")
+    MESSAGE (STATUS "Player libraries will be in lib64")
+ELSE (PLAYER_PROC_64BIT)
+    SET (PLAYER_LIBRARY_INSTALL_DIR "lib")
+    MESSAGE (STATUS "Player libraries will be in lib")
+ENDIF (PLAYER_PROC_64BIT)
+
