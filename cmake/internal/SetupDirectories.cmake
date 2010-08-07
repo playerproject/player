e63fbc9ce87631584deb2a7dfcb41109599869c6
Rich Mattes <richmattes@gmail.com>
2010-08-07 04:31:39 +0000

    Added configurable library install dir suffix to override architecture detection

--- cmake/internal/SetupDirectories.cmake
+++ cmake/internal/SetupDirectories.cmake
@@ -12,10 +12,13 @@ STRING (TOLOWER ${PROJECT_NAME} projectNameLower)
 SET (PLAYER_INCLUDE_INSTALL_DIR "include/${projectNameLower}-${PLAYER_MAJOR_VERSION}.${PLAYER_MINOR_VERSION}")
 
 IF (PLAYER_PROC_64BIT)
-    SET (PLAYER_LIBRARY_INSTALL_DIR "lib64")
-    MESSAGE (STATUS "Player libraries will be in lib64")
+    SET (LIB_SUFFIX "64" CACHE STRING "Suffix for library installation directory")
 ELSE (PLAYER_PROC_64BIT)
-    SET (PLAYER_LIBRARY_INSTALL_DIR "lib")
-    MESSAGE (STATUS "Player libraries will be in lib")
+    SET (LIB_SUFFIX "" CACHE STRING "Suffix for library installation directory")
 ENDIF (PLAYER_PROC_64BIT)
+
+SET (PLAYER_LIBRARY_INSTALL_DIR "lib${LIB_SUFFIX}")
+
+MESSAGE (STATUS "Headers will be installed to ${CMAKE_INSTALL_PREFIX}/${PLAYER_INCLUDE_INSTALL_DIR}")
+MESSAGE (STATUS "Libraries will be installed to ${CMAKE_INSTALL_PREFIX}/${PLAYER_LIBRARY_INSTALL_DIR}")
 
