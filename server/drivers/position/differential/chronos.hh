f2711b8ec1958c95e4f8ff338fc1a761dd2454cf
Rich Mattes <richmattes@gmail.com>
2010-12-11 21:25:28 +0000

    Applied patch #3134439: differential driver 2xPosition1d --> position2d

---
+++ server/drivers/position/differential/chronos.hh
@@ -0,0 +1,42 @@
+/*
+ *  Player - One Hell of a Robot Server
+ *  Copyright (C) 2010
+ *     Alejandro R. Mosteo
+ *
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _chronos_
+#define _chronos_
+
+namespace driver_differential {
+
+class Chronos
+  {
+  public:
+    Chronos ( double seconds_since_epoch = now() );
+    double elapsed ( void ) const;
+    void reset ( void );
+
+  private:
+    double clock_;
+
+    static double now ( void ) ;
+  };
+
+}
+
+#endif
